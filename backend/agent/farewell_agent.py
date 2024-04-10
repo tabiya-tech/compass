509734043808e9b53f384d61e01160739fde8f4b
zoharai <zohar.aizenbud@gmail.com>
2024-04-10 18:54:53 +0300

    first version of refactoring agents - - updating API with history - refactoring welcome_agent - fix missing quotes from json schema instructions that caused a model to respond with malformed json
    COM-107

--- backend/agent/farewell_agent.py
+++ backend/agent/farewell_agent.py
@@ -1,17 +1,11 @@
 import json
 import logging
+from textwrap import dedent
 
-from langchain.chains.llm import LLMChain
-from langchain.memory import ConversationBufferMemory
-from langchain_core.prompts import (
-    ChatPromptTemplate,
-    MessagesPlaceholder,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
 from langchain_google_vertexai import ChatVertexAI
 
-from agent.agent_types import AgentInput, AgentOutput, AgentType, Agent
+from agent.agent_types import AgentInput, AgentOutput, Agent, AgentType, ConversationHistory, \
+    ConversationHistoryFormatter
 from agent.prompt_reponse_template import ModelResponse, get_json_response_instructions
 
 logger = logging.getLogger(__name__)
@@ -19,57 +13,38 @@ logger = logging.getLogger(__name__)
 
 class SimpleFarewellAgent(Agent):
 
-    async def reset(self):
-        """
-        This agent does not have any state to reset
-        """
-
-    async def execute(self, user_input: AgentInput) -> AgentOutput:
+    async def execute(self, user_input: AgentInput, history: ConversationHistory) -> AgentOutput:
+        print(user_input.message + " from FarewellAgent")
         return AgentOutput(message_for_user="FarewellAgent done", finished=True, agent_type=AgentType.FAREWELL_AGENT)
 
 
 class FarewellAgent(Agent):
     def __init__(self):
+        self._agent_type = AgentType.FAREWELL_AGENT
         # Define the response part of the prompt with some example responses
         response_part = get_json_response_instructions([
             ModelResponse(message="Have a nice day", finished=True),
             ModelResponse(message="YOLO!", finished=True),
         ])
 
-        prompt = ChatPromptTemplate.from_messages([
-            SystemMessagePromptTemplate.from_template(f"""
-            You are a checkout receptionist at a job counseling agency.
-            Your task is to say goodbye to the user and end the conversation. 
-                        
-            {response_part}
-            
+        self._prompt = dedent(f"""\
+            You are a {self._agent_type.value} at a job counseling agency.
+            Your task is to say goodbye to the user and end the conversation.
+            """)+ '\n' + response_part + '\n' + dedent("""\
             Farewell the user with a warm goodbye.
-            
-            """),
-            MessagesPlaceholder(variable_name="chat_history"),
-            HumanMessagePromptTemplate.from_template("{user_input}")
-        ])
-        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+            """)
+        self._chain = ChatVertexAI(model_name="gemini-pro")
 
-        llm = ChatVertexAI(model_name="gemini-pro", convert_system_message_to_human=True)
-        self._chain = LLMChain(
-            llm=llm,
-            prompt=prompt,
-            verbose=False,
-            memory=memory
-        )
-
-    async def reset(self):
-        self._chain.memory.clear()
-
-    async def execute(self, user_input: AgentInput) -> AgentOutput:
-        conversation = await self._chain.ainvoke({"user_input": user_input.message})
+    async def execute(self, user_input: AgentInput, history: ConversationHistory) -> AgentOutput:
+        model_input = self._prompt + "\n" + ConversationHistoryFormatter.format_for_prompt(
+            history) + "\nUser: " + user_input.message
+        conversation = await self._chain.ainvoke(model_input)
         try:
-            last: ModelResponse = json.loads(conversation["text"])
+            last: ModelResponse = json.loads(conversation.content)
         except json.JSONDecodeError as e:
             logger.exception(e)
             last = {
-                "message": conversation["text"],
+                "message": str(conversation.content),
                 "finished": False
             }
 
@@ -78,5 +53,5 @@ class FarewellAgent(Agent):
                                agent_type=AgentType.FAREWELL_AGENT)
         return response
 
-    def get_chain(self) -> LLMChain:
+    def get_chain(self) -> ChatVertexAI:
         return self._chain
