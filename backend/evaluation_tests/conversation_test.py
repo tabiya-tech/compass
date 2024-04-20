358bab8173f87ce50b494f79b9897fa8606c7405
Benisis, Apostolos <apostolos@benisis.de>
2024-04-20 21:07:45 +0300

    Move pytest.ini into the backend project, where it belongs.
    Mark evaluation tests and exclude them from the CI/CD.
    
    Improve documentation.
    
    Fix minor linter error

--- backend/evaluation_tests/conversation_test.py
+++ backend/evaluation_tests/conversation_test.py
@@ -6,10 +6,10 @@ from io import TextIOBase
 from textwrap import dedent
 from typing import Callable
 
-from tqdm import tqdm
-import pytest as pytest
+import pytest
 import vertexai
 from dotenv import load_dotenv
+from tqdm import tqdm
 from vertexai.generative_models import GenerativeModel
 
 from app.server import welcome
@@ -19,7 +19,9 @@ from evaluators.criteria_evaluator import CriteriaEvaluator
 
 
 @pytest.mark.asyncio
+@pytest.mark.evaluation_test
 async def test_conversation():
+    """Test for CONCISENESS: 'young student from Kenya trying to find a job'"""
     load_dotenv()
     test_case = "e2e_test"
     prompt = dedent("""
@@ -33,16 +35,18 @@ async def test_conversation():
     model = GenerativeModel(
         "gemini-1.0-pro",
         system_instruction=[prompt])
-    chat = model.start_chat()
+    simulated_user = model.start_chat()
     evaluation_result = TestEvaluationRecord(simulated_user_prompt=prompt, test_case=test_case)
     user_output = ""
 
     # TODO(kingam): Also finish the conversation when Compass is done.
-    for i in tqdm(range(0, 5), desc="Conversation progress"):
+    for _ in tqdm(range(0, 5), desc="Conversation progress"):
+        # Get a response from the evaluated agent
         agent_output = (await welcome(user_input=user_output)).last.message_for_user
         evaluation_result.add_conversation_record(
             ConversationRecord(message=agent_output, actor=Actor.EVALUATED_AGENT))
-        user_output = chat.send_message(agent_output, stream=False).text
+        # Get a response from the simulated user
+        user_output = simulated_user.send_message(agent_output, stream=False).text
         evaluation_result.add_conversation_record(
             ConversationRecord(message=user_output, actor=Actor.SIMULATED_USER))
 
@@ -53,7 +57,6 @@ async def test_conversation():
     #  Currently much information is printed in the console. I found it more useful to see the progress
     #  of the conversation as it take long and I am not sure when it will finish.
     pprint.pprint(json.loads(evaluation_result.to_json()), indent=4)
-
     base_path = os.path.dirname(__file__) + "/test_output/" + test_case + "_" + (str(time.time()))
     # Save the evaluation result to a json file
     save_to_file(base_path + '.json',
@@ -63,6 +66,11 @@ async def test_conversation():
 
 
 def save_to_file(file_path: str, callback: Callable[[TextIOBase], None]):
+    """
+    Save contents to a file.
+    :param file_path: The path to the file, including the file name. If the paths do not exist, they will be created.
+    :param callback: A callback function that should be called to write the content to the file.
+    """
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, 'w', encoding='utf-8') as f:
         callback(f)
