583c0ba3653ff719525bf3ffef6874bf112bd174
Apostolos Benisis <apostolos.benisis@tabiya.org>
2024-04-24 11:01:50 +0300

    chore(backend): Drop usage of langchain and use backend/common_libs/llm/gemini.py instead.

--- backend/app/agent/skill_explore_agent.py
+++ backend/app/agent/skill_explore_agent.py
@@ -1,37 +1,17 @@
-import logging
 from textwrap import dedent
 
-from langchain_google_vertexai import ChatVertexAI
-
-from app.agent.agent import Agent
-from app.agent.agent_types import AgentInput, AgentOutput, AgentType
+from app.agent.agent import SimpleLLMAgent
+from app.agent.agent_types import AgentType
 from app.agent.prompt_reponse_template import ModelResponse, get_json_response_instructions, \
     get_conversation_finish_instructions
-from app.conversation_memory.conversation_formatter import ConversationHistoryFormatter
-from app.conversation_memory.conversation_memory_manager import ConversationHistory
-from common_libs.text_formatters.extract_json import extract_json, ExtractJSONError
-
-logger = logging.getLogger(__name__)
 
 
-class SimpleSkillExplorerAgent(Agent):
-    """
-    Simple agent that provides a simple response, should be used for testing purposes only
-    """
-
-    async def execute(self, user_input: AgentInput, history: ConversationHistory) -> AgentOutput:
-        print(user_input.message + " from SkillExplorerAgent")
-        return AgentOutput(message_for_user="SkillExplorerAgent done", finished=True,
-                           agent_type=AgentType.SKILL_EXPLORER_AGENT)
-
-
-class SkillExplorerAgent(Agent):
+class SkillExplorerAgent(SimpleLLMAgent):
     """
     Agent that explores the skills of the user and provides a response based on the task
     """
 
     def __init__(self):
-        self._agent_type = AgentType.SKILL_EXPLORER_AGENT
         # Define the response part of the prompt with some example responses
         response_part = get_json_response_instructions([
             ModelResponse(message="Tell about the kind of jobs you are interested in.", finished=False),
@@ -43,43 +23,34 @@ class SkillExplorerAgent(Agent):
             than 5 skills.
             When the conversation concludes"""))
 
-        self._prompt = dedent(f"""\
-                You are a {self._agent_type.value} at a skills exploration agency who loves to help people identify 
-                their skills and competencies! 
-                In a friendly tone ask the user about their previous experiences, to  identify skills they might have. 
-                When the user mentions a skill, ask them to elaborate on it, once the user has explained the skill, 
-                repeat the process.
-                
-                If the user is unsure, you can use the topics from the the _TOPICS_ section bellow to help the user 
-                get started.
-                
-                _TOPICS_:
-                    previous job experiences
-                    volunteering work
-                    hobbies 
-                    enjoy doing in their free time
-                    skills they have developed in their previous jobs
-                    help they offered to friends or family members
-                
-                If you are unsure or the user enters information that is not explicitly related to skills exploration 
-                counseling,
-                say "Sorry, this seems to be irrelevant to our conversation, please stay focused."
-                """) + '\n' + response_part + '\n' + finish_instructions
+        system_instructions_template = dedent("""\
+            You are a skill exploration counselor at a skills exploration agency who loves to help people identify 
+            their skills and competencies! 
+            In a friendly tone ask the user about their previous experiences, to  identify skills they might have. 
+            When the user mentions a skill, ask them to elaborate on it, once the user has explained the skill, 
+            repeat the process.
+            
+            If the user is unsure, you can use the topics from the the _TOPICS_ section bellow to help the user 
+            get started.
+            
+            _TOPICS_:
+                previous job experiences
+                volunteering work
+                hobbies 
+                enjoy doing in their free time
+                skills they have developed in their previous jobs
+                help they offered to friends or family members
+            
+            If you are unsure or the user enters information that is not explicitly related to skills exploration 
+            counseling,
+            say "Sorry, this seems to be irrelevant to our conversation, please stay focused."
+            
+            {response_part}
+            
+            {finish_instructions}             
+            """)
 
-        self._chain = ChatVertexAI(model_name="gemini-pro")
-
-    async def execute(self, user_input: AgentInput, history: ConversationHistory) -> AgentOutput:
-        model_input = self._prompt + "\n" + ConversationHistoryFormatter.format_for_prompt(
-            history) + "\nUser: " + user_input.message
-        conversation = await self._chain.ainvoke(model_input)
-
-        try:
-            last: ModelResponse = extract_json(conversation.content, ModelResponse)
-        except ExtractJSONError:
-            logger.warning("Error extracting JSON from conversation content '%s'", conversation.content, exc_info=True)
-            last = ModelResponse(message=str(conversation.content), finished=False)
-
-        response = AgentOutput(message_for_user=last.message,
-                               finished=last.finished,
-                               agent_type=AgentType.SKILL_EXPLORER_AGENT)
-        return response
+        system_instructions = system_instructions_template.format(response_part=response_part,
+                                                                  finish_instructions=finish_instructions)
+        super().__init__(agent_type=AgentType.SKILL_EXPLORER_AGENT,
+                         system_instructions=system_instructions)
--- backend/app/agent/welcome_agent.py
+++ backend/app/agent/welcome_agent.py
@@ -1,36 +1,17 @@
-import logging
 from textwrap import dedent
 
-from langchain_google_vertexai import ChatVertexAI
-
-from app.agent.agent import Agent
-from app.agent.agent_types import AgentInput, AgentOutput, AgentType
+from app.agent.agent import SimpleLLMAgent
+from app.agent.agent_types import AgentType
 from app.agent.prompt_reponse_template import ModelResponse, get_json_response_instructions, \
     get_conversation_finish_instructions
-from app.conversation_memory.conversation_formatter import ConversationHistoryFormatter
-from app.conversation_memory.conversation_memory_manager import ConversationHistory
-from common_libs.text_formatters.extract_json import extract_json, ExtractJSONError
-
-logger = logging.getLogger(__name__)
 
 
-class SimpleWelcomeAgent(Agent):
-    """
-    Simple agent that provides a simple response, should be used for testing purposes only
-    """
-
-    async def execute(self, user_input: AgentInput, history: ConversationHistory) -> AgentOutput:
-        print(user_input.message + " from WelcomeAgent")
-        return AgentOutput(message_for_user="WelcomeAgent done", finished=True, agent_type=AgentType.WELCOME_AGENT)
-
-
-class WelcomeAgent(Agent):
+class WelcomeAgent(SimpleLLMAgent):
     """
     Agent that welcomes the user and provides a response based on the task
     """
 
     def __init__(self):
-        self._agent_type = AgentType.WELCOME_AGENT
         # Define the response part of the prompt with some example responses
         response_part = get_json_response_instructions([
             ModelResponse(message="Welcome! Are you ready to begin?", finished=False),
@@ -41,8 +22,8 @@ class WelcomeAgent(Agent):
             'When the user indicates that they are ready to start the counseling session, ' +
             'or when the user asks to start the counseling session')
 
-        self._prompt = dedent(f"""\
-        You are a {self._agent_type.value} at a skills exploration agency. 
+        system_instructions_template = dedent("""\
+        You are a Receptionist at a skills exploration agency. 
         Your task is to :
            - welcome the user
            - introduce them to the exploration process
@@ -65,22 +46,13 @@ class WelcomeAgent(Agent):
             the exploration session will begin. 
             During that session the user will be asked questions to help them explore and discover their skills.
             Once the user has completed the session, they will be provided with a list skills 
-            explored during the session. 
-        """) + '\n' + response_part + '\n' + dedent("""\
-        """) + '\n' + finish_instructions
-        self._chain = ChatVertexAI(model_name="gemini-pro")
-
-    async def execute(self, user_input: AgentInput, history: ConversationHistory) -> AgentOutput:
-        model_input = self._prompt + "\n" + ConversationHistoryFormatter.format_for_prompt(
-            history) + "\nUser: " + user_input.message
-        conversation = await self._chain.ainvoke(model_input)
-        try:
-            last: ModelResponse = extract_json(conversation.content, ModelResponse)
-        except ExtractJSONError:
-            logger.warning("Error extracting JSON from conversation content '%s'", conversation.content, exc_info=True)
-            last = ModelResponse(message=str(conversation.content), finished=False)
-
-        response = AgentOutput(message_for_user=last.message,
-                               finished=last.finished,
-                               agent_type=AgentType.WELCOME_AGENT)
-        return response
+            explored during the session.
+        
+        {response_part}
+        
+        {finish_instructions}             
+        """)
+        system_instructions = system_instructions_template.format(response_part=response_part,
+                                                                  finish_instructions=finish_instructions)
+        super().__init__(agent_type=AgentType.WELCOME_AGENT,
+                         system_instructions=system_instructions)
