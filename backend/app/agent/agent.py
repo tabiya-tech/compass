93b583ce0721704f10dffc78226d5f2f6908c5a5
zoharai <zohar.aizenbud@gmail.com>
2024-05-06 17:26:30 +0300

    feat(backend): Enrich agentOutput with agent response time and llm token counts COM-148

--- backend/app/agent/agent.py
+++ backend/app/agent/agent.py
@@ -1,7 +1,8 @@
 import logging
+import time
 from abc import ABC, abstractmethod
 
-from app.agent.agent_types import AgentInput, AgentOutput, AgentType
+from app.agent.agent_types import AgentInput, AgentOutput, AgentType, LLMStats
 from app.agent.prompt_reponse_template import ModelResponse
 from app.conversation_memory.conversation_formatter import ConversationHistoryFormatter
 from app.conversation_memory.conversation_memory_manager import ConversationContext
@@ -45,6 +46,8 @@ class SimpleLLMAgent(Agent):
         self._logger = logging.getLogger(self.__class__.__name__)
 
     async def execute(self, user_input: AgentInput, context: ConversationContext) -> AgentOutput:
+        agent_start_time = time.time()
+        llm_stats_list: list[LLMStats] = []
         if user_input.message == "":
             # If the user input is empty, set it to "(silence)"
             # This is to avoid the agent failing to respond to an empty input
@@ -55,26 +58,37 @@ class SimpleLLMAgent(Agent):
         model_response: ModelResponse | None = None
         while not success and attempt_count < _MAX_ATTEMPTS:
             attempt_count += 1
+            llm_start_time = time.time()
             llm_response = await self._llm.generate_content_async(
                 contents=ConversationHistoryFormatter.format_for_agent_generative_prompt(context, msg)
             )
+            llm_end_time = time.time()
+            llm_stats = LLMStats(prompt_token_count=llm_response.prompt_token_count,
+                                 response_token_count=llm_response.response_token_count,
+                                 response_time_in_sec=round(llm_end_time - llm_start_time, 2))
+            response_text = llm_response.text
             try:
-                model_response = extract_json(llm_response, ModelResponse)
+                model_response = extract_json(response_text, ModelResponse)
                 success = True
             except ExtractJSONError:
-                log_message = "Attempt %d failed to extract JSON from conversation content: '%s'"
+                log_message = (f"Attempt {attempt_count} failed to extract JSON "
+                               f"from conversation content: '{response_text}")
+                llm_stats.error = log_message
                 if attempt_count == _MAX_ATTEMPTS:
                     # The agent failed to respond with a JSON object after the last attempt,
-                    self._logger.error(log_message, attempt_count, llm_response)
+                    self._logger.error(log_message)
                     # And set the response to the model output and hope that the conversation can continue
-                    model_response = ModelResponse(message=str(llm_response), finished=False,
+                    model_response = ModelResponse(message=response_text, finished=False,
                                                    reasoning="Failed to respond with JSON")
                 else:
-                    self._logger.warning(log_message, attempt_count, llm_response)
+                    self._logger.warning(log_message)
             # Any other exception should be caught and logged
             except Exception as e:  # pylint: disable=broad-except
                 self._logger.error("An error occurred while requesting a response from the model: %s",
                                    e, exc_info=True)
+                llm_stats.error = str(e)
+            finally:
+                llm_stats_list.append(llm_stats)
 
         # If it was not possible to get a model response, set the response to a default message
         if model_response is None:
@@ -85,10 +99,13 @@ class SimpleLLMAgent(Agent):
 
         self._logger.debug("Model input: %s", user_input.message)
         self._logger.debug("Model output: %s", model_response)
+        agent_end_time = time.time()
         response = AgentOutput(message_for_user=model_response.message,
                                finished=model_response.finished,
                                reasoning=model_response.reasoning,
-                               agent_type=self._agent_type)
+                               agent_type=self._agent_type,
+                               agent_response_time_in_sec=round(agent_end_time - agent_start_time, 2),
+                               llm_stats=llm_stats_list)
         return response
 
 
@@ -98,7 +115,7 @@ class _SimpleStatelessChatLLMAgent(Agent):
     It seems that it performs equally well as the SimpleLLMAgent, but has some internal overhead as it
     uses an internal chat session to manage the conversation history in a similar way as thew ConversationMemoryManager.
     The code is kept here for reference, but it is not used in the current implementation.
-    TODO: Remove this class in the future.   
+    TODO: Remove this class in the future.
     """
 
     def __init__(self, *, agent_type: AgentType, system_instructions: str, config: LLMConfig = LLMConfig()):
