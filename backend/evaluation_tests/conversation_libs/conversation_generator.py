87be8e7b753dcba215b0cf7b9183a7838586a576
kingam <kingam@google.com>
2024-05-23 14:22:18 +0200

    Move comment.

--- backend/evaluation_tests/conversation_libs/conversation_generator.py
+++ backend/evaluation_tests/conversation_libs/conversation_generator.py
@@ -25,9 +25,9 @@ async def generate(*, max_iterations: int,
         # Get a response from the evaluated agent.
         agent_output = await execute_evaluated_agent(agent_input=AgentInput(message=simulated_user_output))
         message_for_user = agent_output.message_for_user
-        # Checks whether the chatbot is done. And finishes the loop if so.
         conversation.append(
             ConversationRecord(message=message_for_user, actor=Actor.EVALUATED_AGENT))
+        # Checks whether the chatbot is done. And finishes the loop if so.
         if is_finished(agent_output=agent_output):
             print(f'Conversation finished earlier, after {i} out of {max_iterations} iterations.')
             break
