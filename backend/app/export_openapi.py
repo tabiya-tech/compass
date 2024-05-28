593fcf721eec02b606059df523954159a57bb789
teppokauppinen-google <127402547+teppokauppinen-google@users.noreply.github.com>
2024-05-28 15:52:06 +0000

    python script to export OpenAPI 3.0 doc. Use 'python3 export_openapi.py', which will create openapi.yaml file

---
+++ backend/app/export_openapi.py
@@ -0,0 +1,13 @@
+from fastapi.openapi.utils import get_openapi
+from server import app
+from yaml import dump
+
+
+with open('openapi.yaml', 'w') as f:
+    dump(get_openapi(
+        title=app.title,
+        version=app.version,
+        openapi_version=app.openapi_version,
+        description=app.description,
+        routes=app.routes
+    ), f)
