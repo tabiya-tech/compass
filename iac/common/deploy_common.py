86623159a643739630316d23290830326e8220c1
zoharai <zohar.aizenbud@gmail.com>
2024-06-09 15:33:46 +0300

    feat(backend) route /api to the api-gateway COM-272
    - route dev.compass.tabiya.tech/api/* -> api-gateway
    - ensure correct order or path-rules
    - adjust frontend to connect to backend via the backend url
    - ensure swagger ui is working
    - add correct CORS origins to the backend
    - assert smoke-tests-backend pass before deploying common infra

--- iac/common/deploy_common.py
+++ iac/common/deploy_common.py
@@ -17,7 +17,9 @@ def _setup_loadbalancer(*, basic_config: ProjectBaseConfig,
                         dns_zone: gcp.dns.ManagedZone,
                         frontend_domain: str,
                         frontend_url: str,
-                        frontend_bucket_name: pulumi.Output[str]):
+                        frontend_bucket_name: pulumi.Output[str],
+                        backend_url: str,
+                        api_gateway_id: pulumi.Output[str]):
     # Create a global IP address for the load balancer
     ipaddress = gcp.compute.GlobalAddress(
         get_resource_name(environment=basic_config.environment, resource="lb-ipaddress"),
@@ -35,7 +37,7 @@ def _setup_loadbalancer(*, basic_config: ProjectBaseConfig,
                       rrdatas=[ipaddress.address])
     # Create a backend service for the frontend bucket
     backend_service_bucket = gcp.compute.BackendBucket(
-        get_resource_name(environment=basic_config.environment, resource="lb-backendservice"),
+        get_resource_name(environment=basic_config.environment, resource="lb-backend-bucket-for-frontend"),
         project=basic_config.project,
         bucket_name=frontend_bucket_name,
         enable_cdn=True,
@@ -44,11 +46,66 @@ def _setup_loadbalancer(*, basic_config: ProjectBaseConfig,
         ),
     )
 
-    # Map <frontend_url>/* -> /* of the frontend bucket
-    parsed_url = urlparse(frontend_url)
+    # create a region network endpoint group for connecting to the api gateway
+    endpoint_group = gcp.compute.RegionNetworkEndpointGroup(
+        get_resource_name(environment=basic_config.environment, resource="lb-endpoint-group"),
+        network_endpoint_type="SERVERLESS",
+        project=basic_config.project,
+        region=basic_config.location,
+        serverless_deployment=gcp.compute.RegionNetworkEndpointGroupServerlessDeploymentArgs(
+            platform="apigateway.googleapis.com",
+            resource=api_gateway_id
+        )
+    )
 
-    port = f"{parsed_url.port}" if parsed_url.port else "80"
-    frontend_path_rule = parsed_url.path.strip("/") + "/*"
+    # Create a backend service for the api gateway
+    api_gateway_backend_service = gcp.compute.BackendService(
+        get_resource_name(environment=basic_config.environment, resource="lb-backend-service-for-api-gateway"),
+        project=basic_config.project,
+        connection_draining_timeout_sec=10,
+        protocol="HTTP",
+        enable_cdn=False,  # the responses will not be cached.
+        load_balancing_scheme="EXTERNAL_MANAGED",
+        backends=[gcp.compute.BackendServiceBackendArgs(group=endpoint_group.id)],
+        log_config=gcp.compute.BackendServiceLogConfigArgs(enable=True),
+        opts=pulumi.ResourceOptions(depends_on=[endpoint_group]),
+    )
+
+    route_action = gcp.compute.URLMapPathMatcherPathRuleRouteActionArgs(
+        url_rewrite=gcp.compute.URLMapPathMatcherPathRuleRouteActionUrlRewriteArgs(
+            path_prefix_rewrite="/",
+        )
+    )
+    # Map <frontend_url>/* -> /* of the frontend bucket
+    frontend_parsed_url = urlparse(frontend_url)
+
+    port = f"{frontend_parsed_url.port}" if frontend_parsed_url.port else "80"
+
+    frontend_parsed_url_path = frontend_parsed_url.path.strip("/")
+    frontend_path_rule = "/" + frontend_parsed_url_path + "/*" if frontend_parsed_url_path else "/*"
+
+    frontend_rule = gcp.compute.URLMapPathMatcherPathRuleArgs(paths=[frontend_path_rule],
+                                                              service=backend_service_bucket.id,
+                                                              route_action=route_action)
+
+    # Map <backend_url>/* -> /* of the backend service
+    backend_parsed_url = urlparse(backend_url)
+    backend_parsed_url_path = backend_parsed_url.path.strip("/")
+    backend_path_rule = "/" + backend_parsed_url_path + "/*" if backend_parsed_url_path else "/*"
+    backend_rule = gcp.compute.URLMapPathMatcherPathRuleArgs(paths=[backend_path_rule,
+                                                                    "/openapi.json"  # Swagger UI will request this file
+                                                                    ],
+                                                             service=api_gateway_backend_service.id,
+                                                             route_action=route_action)
+
+    # Swagger UI will request /openapi.json, so we need to rewrite the path to the correct one
+    backend_openapi_rule = gcp.compute.URLMapPathMatcherPathRuleArgs(paths=["/openapi.json"],
+                                                                     service=api_gateway_backend_service.id,
+                                                                     )
+
+    if backend_parsed_url_path == frontend_parsed_url_path:
+        pulumi.error("The frontend and backend paths cannot be the same")
+        sys.exit(1)
 
     http_url_map = gcp.compute.URLMap(
         get_resource_name(environment=basic_config.environment, resource="http-urlmap"),
@@ -58,13 +115,12 @@ def _setup_loadbalancer(*, basic_config: ProjectBaseConfig,
             gcp.compute.URLMapHostRuleArgs(
                 hosts=[frontend_domain],
                 path_matcher="all-paths",
-            )
+            ),
         ],
         path_matchers=[gcp.compute.URLMapPathMatcherArgs(
             name="all-paths",
             default_service=backend_service_bucket.id,
-            path_rules=[gcp.compute.URLMapPathMatcherPathRuleArgs(paths=[frontend_path_rule],
-                                                                  service=backend_service_bucket.id)],
+            path_rules=[backend_rule, backend_openapi_rule, frontend_rule],
         )],
         opts=pulumi.ResourceOptions(depends_on=[backend_service_bucket]),
     )
@@ -104,12 +160,18 @@ def deploy_common(project: str,
                   environment: str,
                   domain_name: str,
                   frontend_domain: str,
-                  frontend_url: str):
+                  frontend_url: str,
+                  backend_url: str):
     basic_config = ProjectBaseConfig(project=project, location=location, environment=environment)
     # Get the frontend bucket name
     frontend_stack_ref = pulumi.StackReference(f"tabiya-tech/compass-frontend/{basic_config.environment}")
-    bucket_name = frontend_stack_ref.get_output("bucket_name")
-    bucket_name.apply(lambda name: print(f"Using frontend bucket name: {name}"))
+    frontend_bucket_name = frontend_stack_ref.get_output("bucket_name")
+    frontend_bucket_name.apply(lambda name: print(f"Using frontend bucket name: {name}"))
+
+    # Get the backend api gateway id
+    backend_stack_ref = pulumi.StackReference(f"tabiya-tech/compass-backend/{basic_config.environment}")
+    api_gateway_id = backend_stack_ref.get_output("apigateway_id")
+    api_gateway_id.apply(lambda id: print(f"Using API gateway id: {id}"))
 
     # Enable the necessary services
     services = enable_services(basic_config=basic_config,
@@ -126,4 +188,6 @@ def deploy_common(project: str,
                         dns_zone=dns_zone,
                         frontend_domain=frontend_domain,
                         frontend_url=frontend_url,
-                        frontend_bucket_name=bucket_name)
+                        frontend_bucket_name=frontend_bucket_name,
+                        backend_url=backend_url,
+                        api_gateway_id=api_gateway_id)
