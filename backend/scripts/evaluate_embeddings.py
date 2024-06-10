58efde7527bb63f4e32fb2e2b6b0861e4460f098
Kinga Mrugala <kingam@google.com>
2024-06-10 12:36:57 +0200

    feat(backend): COM-204, COM-205 Update the script to generate skill embeddings.  (#74)
    * Add skill generation and batch evaluation.

--- backend/scripts/evaluate_embeddings.py
+++ backend/scripts/evaluate_embeddings.py
@@ -3,23 +3,26 @@ import os
 from typing import List, Optional, Tuple
 
 import vertexai
-from datasets import load_dataset
-from motor.motor_asyncio import AsyncIOMotorClient
+from datasets import load_dataset, Features, Value, VerificationMode
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 from tqdm import tqdm
 
 from app.vector_search.embeddings_model import GoogleGeckoEmbeddingService, EmbeddingService
-from app.vector_search.esco_entities import OccupationEntity
 
 OCCUPATION_REPO_ID = "tabiya/hahu_test"
 OCCUPATION_FILENAME = "redacted_hahu_test_with_id.csv"
+SKILL_REPO_ID = "tabiya/esco_skills_test"
+SKILL_FILENAME = "data/processed_skill_test_set_with_id.parquet"
 # TODO: Load the database name and collection name from the environment variables.
 DATABASE_NAME = 'compass-test'
-EMBEDDINGS_COLLECTION = 'occupationmodelsembeddings'
+OCCUPATION_EMBEDDINGS_COLLECTION = 'occupationmodelsembeddings'
+SKILLS_EMBEDDINGS_COLLECTION = 'skillsmodelsembeddings'
 
 
 # TODO: Use the OccupationSearchService to perform a similarity search on the vector store, once we migrate everything
 #  to the new structure.
-async def _search(embedding_service: EmbeddingService, query: str, k: int = 5) -> List[OccupationEntity]:
+async def _search(embedding_service: EmbeddingService, collection: AsyncIOMotorCollection, query: str, k: int = 5) -> \
+        List[dict]:
     """
         Perform a similarity search on the vector store. It uses the default similarity search set during vector
         generation.
@@ -50,22 +53,7 @@ async def _search(embedding_service: EmbeddingService, query: str, k: int = 5) -
         {"$sort": {"score": -1}},
         {"$limit": k},
     ]
-    return [_to_entity(entry) async for entry in collection.aggregate(pipeline)]
-
-
-def _to_entity(doc: dict) -> OccupationEntity:
-    """
-    Convert a Document object to an OccupationEntity object.
-    """
-
-    return OccupationEntity(
-        id=str(doc.get("_id", "")),
-        UUID=doc.get("UUID", ""),
-        code=doc.get("code", ""),
-        preferredLabel=doc.get("preferredLabel", ""),
-        description=doc.get("description", ""),
-        altLabels=doc.get("altLabels", []),
-    )
+    return [entry async for entry in collection.aggregate(pipeline)]
 
 
 def _precision_at_k(prediction: List[List[str]], true: List[List[str]], k: Optional[int] = None):
@@ -111,21 +99,24 @@ def _get_all_metrics(predictions: List[List[str]], true_values: List[List[str]],
     return rec_at_k, prec_at_k, f_score_at_k
 
 
-async def _get_predictions(embedding_service: EmbeddingService, queries: List[str], k: int = 10):
+async def _get_predictions(embedding_service: EmbeddingService, collection: AsyncIOMotorCollection, queries: List[str],
+                           evaluated_field: str = "code", k: int = 10):
     # We could run predictions in batches to make it quicker, however there is a quota limit on the gecko embeddings
     # API. We are running it sequentially to avoid hitting the limit.
     predictions = []
     for query in tqdm(queries):
-        result = await _search(embedding_service, query, k)
-        predictions.append([e.code for e in result])
+        result = await _search(embedding_service, collection, query, k)
+        predictions.append([e[evaluated_field] for e in result])
     return predictions
 
 
-async def get_metrics(embedding_service: EmbeddingService, ground_truth: List[str], synthetic_queries: List[str],
+async def get_metrics(embedding_service: EmbeddingService, collection: AsyncIOMotorCollection, ground_truth: List[str],
+                      synthetic_queries: List[str], evaluated_field: str = "code",
                       k: int = 10):
     """ Evaluate the embeddings using ground truth data and synthetic queries."""
-    predictions = await _get_predictions(embedding_service, synthetic_queries, k)
+    predictions = await _get_predictions(embedding_service, collection, synthetic_queries, evaluated_field, k)
     ground_truth = [[elem] for elem in ground_truth]
+    print(f"Metrics for the embeddings: {collection.name}")
     for k in [1, 3, 5, 10]:
         recall, precision, f_score = _get_all_metrics(predictions, ground_truth, k)
         print(f"K = {k}, recall: {recall}, precision: {precision}, f_score: {f_score}")
@@ -134,10 +125,30 @@ async def get_metrics(embedding_service: EmbeddingService, ground_truth: List[st
 if __name__ == "__main__":
     vertexai.init()
     compass_db = AsyncIOMotorClient(os.getenv('MONGODB_URI')).get_database(DATABASE_NAME)
-    collection = compass_db[EMBEDDINGS_COLLECTION]
     gecko_embedding_service = GoogleGeckoEmbeddingService()
-    # TODO: Update this script to also evaluate the skill embeddings.
-    dataset = load_dataset(OCCUPATION_REPO_ID, data_files=[OCCUPATION_FILENAME],
-                           token=os.environ["HF_ACCESS_TOKEN"]).get("train")
-    asyncio.get_event_loop().run_until_complete(get_metrics(gecko_embedding_service, dataset["esco_code"],
-                                                            dataset["synthetic_query"]))
+    occupation_dataset = load_dataset(OCCUPATION_REPO_ID, data_files=[OCCUPATION_FILENAME],
+                                      token=os.environ["HF_ACCESS_TOKEN"]).get("train")
+    # Load the skill dataset. The columns are not consistent with the definition in the dataset so we need to override
+    # it and disable verification.
+    skill_dataset = load_dataset(SKILL_REPO_ID, data_files=[SKILL_FILENAME], token=os.environ["HF_ACCESS_TOKEN"],
+                                 features=Features(
+                                     {
+                                         "label": Value(dtype="string"),
+                                         "sentence": Value(dtype="string"),
+                                         "span": Value(dtype="string"),
+                                         "sub_span": Value(dtype="string"),
+                                         "ID": Value(dtype="string"),
+                                         "UUID": Value(dtype="string"),
+                                         "synthetic_query": Value(dtype="string"),
+                                         "__index_level_0__": Value(dtype="int64"),
+                                     }
+                                 ),
+                                 split="train",
+                                 verification_mode=VerificationMode.NO_CHECKS)
+    asyncio.get_event_loop().run_until_complete(
+        asyncio.gather(*[get_metrics(gecko_embedding_service, compass_db[OCCUPATION_EMBEDDINGS_COLLECTION],
+                                     occupation_dataset["esco_code"],
+                                     occupation_dataset["synthetic_query"], evaluated_field="code"),
+                         get_metrics(gecko_embedding_service, compass_db[SKILLS_EMBEDDINGS_COLLECTION],
+                                     skill_dataset["label"],
+                                     skill_dataset["synthetic_query"], evaluated_field="preferredLabel")]))
