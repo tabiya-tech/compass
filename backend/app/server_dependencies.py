07c635f64be890edb3775ff378f9691dd8327faf
kingam <kingam@google.com>
2024-06-10 17:08:45 +0200

    Update similarity search to use multiple embeddings.

--- backend/app/server_dependencies.py
+++ backend/app/server_dependencies.py
@@ -1,8 +1,10 @@
-import os
-
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
-_mongo_db: AsyncIOMotorDatabase = AsyncIOMotorClient(os.getenv("MONGODB_URI")).get_database("compass-poc")
+from common_libs.environment_settings.mongo_db_settings import MongoDbSettings
+
+_settings = MongoDbSettings()
+
+_mongo_db: AsyncIOMotorDatabase = AsyncIOMotorClient(_settings.mongodb_uri).get_database(_settings.database_name)
 
 
 def get_mongo_db() -> AsyncIOMotorDatabase:
