9af5874b1053966402043f5af5f53ea05b3d86ae
kingam <kingam@google.com>
2024-06-12 15:23:21 +0200

    Delete unused code.

--- backend/conftest.py
+++ backend/conftest.py
@@ -31,14 +31,6 @@ def event_loop():
     yield loop
     loop.close()
 
-# @pytest.fixture(scope="session")
-# def event_loop():
-#
-#     """Create an instance of the default event loop for each test case."""
-#     loop = asyncio.get_event_loop_policy().new_event_loop()
-#     yield loop
-#     loop.close()
-
 
 @pytest.fixture
 def fake_conversation_context() -> FakeConversationContext:
