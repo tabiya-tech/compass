931d080f3043980ddbf4ed861638b5fe55df7e02
teppokauppinen-google <127402547+teppokauppinen-google@users.noreply.github.com>
2024-06-12 14:48:51 +0100

    fix(iac): fix OpenAPI export (#88)
    * fix(iac): fix OpenAPI export
    
    * remove JWT_Auth for now as UI does not support auth
    
    * newline

--- backend/scripts/convert_to_openapi2.py
+++ backend/scripts/convert_to_openapi2.py
@@ -3,25 +3,32 @@ import yaml
 # Simplified OpenAPI 3.1 to OpenAPI 2.0 converter
 # This will only convert the paths that are appended to OpenAPI 2.0 template
 
-with open('openapi3.yaml', 'r') as f:
-    openapi3 = yaml.load(f, Loader=yaml.SafeLoader)
+def convert():
+    with open('openapi3.yaml', 'r') as f:
+        openapi3 = yaml.load(f, Loader=yaml.SafeLoader)
 
-with open('openapi2_template.yaml', 'r') as f:
-    openapi2 = yaml.load(f, Loader=yaml.SafeLoader)
+    with open('openapi2_template.yaml', 'r') as f:
+        openapi2 = yaml.load(f, Loader=yaml.SafeLoader)
 
-for path in openapi3['paths']:
-    for method in openapi3['paths'][path]:
-        if 'parameters' in openapi3['paths'][path][method]:
-            for param in openapi3['paths'][path][method]['parameters']:
-                schema = param.pop('schema', {type: None})
-                param['type'] = schema['type']
+    for path in openapi3['paths']:
+        for method in openapi3['paths'][path]:
+            if 'parameters' in openapi3['paths'][path][method]:
+                for param in openapi3['paths'][path][method]['parameters']:
+                    schema = param.pop('schema', {type: None})
+                    param['type'] = schema['type']
 
-        # remove response contents as not required in GCP API Gateway configs
-        if 'responses' in openapi3['paths'][path][method]:
-            for response in openapi3['paths'][path][method]['responses']:
-                openapi3['paths'][path][method]['responses'][response].pop('content',None)
+            # remove response contents as not required in GCP API Gateway configs
+            if 'responses' in openapi3['paths'][path][method]:
+                for response in openapi3['paths'][path][method]['responses']:
+                    openapi3['paths'][path][method]['responses'][response].pop('content',None)
 
-openapi2['paths'] = openapi3['paths']
+            # remove JWT_auth for now as UI does not support it
+            if 'security' in openapi3['paths'][path][method]:
+                for security in openapi3['paths'][path][method]['security']:
+                    if 'JWT_auth' in security:
+                        openapi3['paths'][path][method].pop('security')
 
-with open('openapi2.yaml', 'w') as f:
-    yaml.dump(openapi2, f)
+    openapi2['paths'] = openapi3['paths']
+
+    with open('openapi2.yaml', 'w') as f:
+        yaml.dump(openapi2, f)
--- backend/scripts/export_openapi2.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-python3 export_openapi.py
-python3 convert_to_openapi2.py
-cp openapi2.yaml ../../iac/backend/config/gcp_api_gateway_config.yaml
