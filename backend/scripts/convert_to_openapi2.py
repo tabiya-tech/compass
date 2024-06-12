deab8896f3648519d256055c80ff34f764a37dcd
teppokauppinen-google <127402547+teppokauppinen-google@users.noreply.github.com>
2024-06-12 12:43:45 +0000

    fix(iac): fix OpenAPI export

--- backend/scripts/convert_to_openapi2.py
+++ backend/scripts/convert_to_openapi2.py
@@ -3,25 +3,26 @@ import yaml
 # Simplified OpenAPI 3.1 to OpenAPI 2.0 converter
 # This will only convert the paths that are appended to OpenAPI 2.0 template
 
-with open('openapi3.yaml', 'r') as f:
-    openapi3 = yaml.load(f, Loader=yaml.SafeLoader)
+def convert():
+    with open('openapi3.yaml', 'r') as f:
+        openapi3 = yaml.load(f, Loader=yaml.SafeLoader)
 
-with open('openapi2_template.yaml', 'r') as f:
-    openapi2 = yaml.load(f, Loader=yaml.SafeLoader)
+    with open('openapi2_template.yaml', 'r') as f:
+        openapi2 = yaml.load(f, Loader=yaml.SafeLoader)
 
-for path in openapi3['paths']:
-    for method in openapi3['paths'][path]:
-        if 'parameters' in openapi3['paths'][path][method]:
-            for param in openapi3['paths'][path][method]['parameters']:
-                schema = param.pop('schema', {type: None})
-                param['type'] = schema['type']
+    for path in openapi3['paths']:
+        for method in openapi3['paths'][path]:
+            if 'parameters' in openapi3['paths'][path][method]:
+                for param in openapi3['paths'][path][method]['parameters']:
+                    schema = param.pop('schema', {type: None})
+                    param['type'] = schema['type']
 
-        # remove response contents as not required in GCP API Gateway configs
-        if 'responses' in openapi3['paths'][path][method]:
-            for response in openapi3['paths'][path][method]['responses']:
-                openapi3['paths'][path][method]['responses'][response].pop('content',None)
+            # remove response contents as not required in GCP API Gateway configs
+            if 'responses' in openapi3['paths'][path][method]:
+                for response in openapi3['paths'][path][method]['responses']:
+                    openapi3['paths'][path][method]['responses'][response].pop('content',None)
 
-openapi2['paths'] = openapi3['paths']
+    openapi2['paths'] = openapi3['paths']
 
-with open('openapi2.yaml', 'w') as f:
-    yaml.dump(openapi2, f)
+    with open('openapi2.yaml', 'w') as f:
+        yaml.dump(openapi2, f)
--- backend/scripts/export_openapi.py
+++ backend/scripts/export_openapi.py
@@ -5,12 +5,12 @@ from fastapi.openapi.utils import get_openapi
 from server import app
 from yaml import dump
 
-
-with open('openapi3.yaml', 'w') as f:
-    dump(get_openapi(
-        title=app.title,
-        version=app.version,
-        openapi_version=app.openapi_version,
-        description=app.description,
-        routes=app.routes
-    ), f)
+def export():
+    with open('openapi3.yaml', 'w') as f:
+        dump(get_openapi(
+            title=app.title,
+            version=app.version,
+            openapi_version=app.openapi_version,
+            description=app.description,
+            routes=app.routes
+        ), f)
