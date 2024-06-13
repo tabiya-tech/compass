a7ee0f91596f02899618f510cb212c1f6acb47b8
Anselme Habumugisha Irumva <andesanselme@gmail.com>
2024-06-13 15:52:41 +0200

    fix: move constants to /app

--- backend/app/users/repositories.py
+++ backend/app/users/repositories.py
@@ -1,14 +1,15 @@
 from app.server_dependencies import get_mongo_db
-from constants.database import Collections
+from app.constants.database import Collections
+
 
 class UserPreferenceRepository:
     collection = get_mongo_db().get_collection(Collections.USER_PREFERENCES)
 
-    def get_user_preference_by_user_id(self, user_id):
-        return self.collection.find_one({"user_id": user_id})
+    async def get_user_preference_by_user_id(self, user_id):
+        return await self.collection.find_one({"user_id": user_id})
 
-    def insert_user_preference(self, user_preference):
-        return self.collection.insert_one(user_preference)
+    async def insert_user_preference(self, user_preference):
+        return await self.collection.insert_one(user_preference)
 
-    def update_user_preference(self, _filter, update):
-        return self.collection.update_one(_filter, update)
+    async def update_user_preference(self, _filter, update):
+        return await self.collection.update_one(_filter, update)
