a521f218d843f947bd9086481b2adbd6c711fd72
zoharai <zohar.aizenbud@gmail.com>
2024-06-16 14:06:35 +0300

    add smoke test to backend-url with domain COM-274 [pulumi up]

--- backend/smoke_test/test_version.py
+++ backend/smoke_test/test_version.py
@@ -15,11 +15,13 @@ def test_version_info():
     # AND the expected version info, a JSON string
     expected_version = os.environ.get("EXPECTED_VERSION_INFO")
     version_json = json.loads(expected_version)
+    # AND the url for getting the version
+    get_version_url = os.environ.get("GET_VERSION_URL")
 
     # WHEN the client requests the version info
     client = httpx.Client(base_url=base_url, timeout=25.0)
     try:
-        response = client.get("/version")
+        response = client.get(get_version_url)
     except httpx.RequestError as e:
         pytest.fail(f"Request failed: {e}")
 
@@ -27,4 +29,8 @@ def test_version_info():
     assert response.status_code == 200
     # AND the response should contain the expected version info
     response_json = response.json()
-    assert version_json.get("sha") == response_json.get("version").get("sha")
+    if "version" in response_json:
+        sha = response_json.get("version").get("sha")
+    else:
+        sha = response_json.get("sha")
+    assert version_json.get("sha") == sha
