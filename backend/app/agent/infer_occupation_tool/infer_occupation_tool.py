e2f3748e4f4b255142653bead0a25d811044cdb2
Francesco Preta <fp627@nyu.edu>
2024-06-26 15:05:06 +0200

    feat(backend): Introduce Infer Occupations Tool

---
+++ backend/app/agent/infer_occupation_tool/infer_occupation_tool.py
@@ -0,0 +1,70 @@
+import asyncio
+import logging
+
+from pydantic import BaseModel
+
+from app.countries import Country
+from app.agent.agent_types import LLMStats
+from app.agent.experience.experience_entity import ExperienceEntity
+from app.agent.infer_occupation_tool._contextualization_llm import _ContextualizationLLM
+
+from app.vector_search.esco_entities import OccupationSkillEntity
+from app.vector_search.esco_search_service import OccupationSkillSearchService
+from common_libs.environment_settings.mongo_db_settings import MongoDbSettings
+
+MONGO_SETTINGS = MongoDbSettings()
+
+
+class InferredOccupationResult(BaseModel):
+    contextualized_title: str
+    esco_occupations: list[OccupationSkillEntity]
+    stats: LLMStats
+
+    class Config:
+        extra = "forbid"
+
+
+class InferOccupationTool:
+
+    def __init__(
+            self,
+            occupation_skill_search_service: OccupationSkillSearchService,
+    ):
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._occupation_skill_search_service = occupation_skill_search_service
+
+    async def execute(self,
+                      country_of_interest: Country,
+                      experience: ExperienceEntity,
+                      top_k: int = 10) -> InferredOccupationResult:
+        """
+        TODO: Add description
+        """
+
+        contextualization_llm = _ContextualizationLLM(country_of_interest, self._logger)
+        contextualized_title, llm_stats = await contextualization_llm.execute(experience)
+        tasks = [self._occupation_skill_search_service.search(contextualized_title, top_k)]
+        if contextualized_title != experience.experience_title:
+            tasks.append(self._occupation_skill_search_service.search(experience.experience_title, top_k))
+
+        list_of_occupation_list = await asyncio.gather(*tasks)
+        occupations_skills = flattern(list_of_occupation_list)
+        return InferredOccupationResult(contextualized_title=contextualized_title,
+                                        esco_occupations=occupations_skills,
+                                        stats=llm_stats
+                                        )
+
+
+def flattern(list_of_occupation_list: list[list[OccupationSkillEntity]]) -> list[OccupationSkillEntity]:
+    # Flatten the list of lists
+    flattened_results: list[OccupationSkillEntity] = [item for sublist in list_of_occupation_list for item in sublist]
+
+    # Filter out duplicates based on uuid
+    seen_uuids = set()
+    unique_results: list[OccupationSkillEntity] = []
+    for item in flattened_results:
+        if item.occupation.UUID not in seen_uuids:
+            seen_uuids.add(item.occupation.UUID)
+            unique_results.append(item)
+
+    return unique_results
---
+++ backend/app/countries.py
@@ -0,0 +1,32 @@
+from enum import Enum
+from textwrap import dedent
+
+
+class Country(Enum):
+    SOUTH_AFRICA = "South Africa"
+    KENYA = "Kenya"
+    FRANCE = "France"
+    UNSPECIFIED = "Unspecified"
+
+
+def get_country_glossary(country: Country) -> str | None:
+    """
+    Get the glossary for a specific country
+    :param country:
+    :return:
+    """
+    return _GLOSSARY.get(country, None)
+
+
+# A glossary of terms that may be used to contextualize tasks
+# eventually this could move to a file or database
+_GLOSSARY = {
+    Country.SOUTH_AFRICA:
+        dedent("""\
+        GDE Brigade member - A member of the Gauteng Department of Education's (GDE) Brigade program.
+        Kotas - A South African street food. 
+        Crew Member - A member of a team but not necessarily in the aviation industry.
+        Ambassador - A person who works ta public relation of a company.
+        Braider - A person who specializes in creating beautiful African hairstyles.
+        """),
+}
