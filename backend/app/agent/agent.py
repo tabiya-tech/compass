1f950c415027d301512c6d0fe0220bff544efa98
Apostolos Benisis <apostolos.benisis@tabiya.org>
2024-06-26 09:01:36 +0300

    feat(backend): Agents and their LLM can return arbitrary typed data.

--- backend/app/agent/agent.py
+++ backend/app/agent/agent.py
@@ -1,18 +1,14 @@
 import logging
-import time
-from abc import ABC, abstractmethod
 
-from app.agent.agent_types import AgentInput, AgentOutput, AgentType, LLMStats
-from app.agent.prompt_reponse_template import ModelResponse
-from app.conversation_memory.conversation_formatter import ConversationHistoryFormatter
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+from app.agent.agent_types import AgentInput, AgentOutput, AgentType
+from app.agent.llm_caller import LLMCaller
+
 from app.conversation_memory.conversation_memory_manager import ConversationContext
-from common_libs.llm.chat_models import GeminiStatelessChatLLM
 from common_libs.llm.generative_models import GeminiGenerativeLLM
 from common_libs.llm.models_utils import LLMConfig, LOW_TEMPERATURE_GENERATION_CONFIG
-from common_libs.text_formatters.extract_json import extract_json, ExtractJSONError
-
-# Number of retries to get a JSON object from the model
-_MAX_ATTEMPTS = 3
 
 
 class Agent(ABC):
@@ -31,13 +27,16 @@ class Agent(ABC):
         raise NotImplementedError()
 
 
-class SimpleLLMAgent(Agent):
+P = TypeVar('P')
+
+
+class SimpleLLMAgent(Agent, Generic[P]):
     """
-    This is a simple agent that uses the GeminiGenerativeLLM to respond to the user input in a conversation.
+    This is a simple stateless agent that uses the GeminiGenerativeLLM to respond to the user input in a conversation.
     """
 
     def __init__(self, *, agent_type: AgentType, system_instructions: str,
-            config: LLMConfig = LLMConfig(generation_config=LOW_TEMPERATURE_GENERATION_CONFIG)):
+                 config: LLMConfig = LLMConfig(generation_config=LOW_TEMPERATURE_GENERATION_CONFIG)):
         self._agent_type = agent_type
         self._llm_config = config
         self._system_instructions = system_instructions
@@ -46,127 +45,16 @@ class SimpleLLMAgent(Agent):
         # does not seem to work well with the model as it does follow the instructions correctly.
         self._llm = GeminiGenerativeLLM(system_instructions=system_instructions, config=config)
         self._logger = logging.getLogger(self.__class__.__name__)
+        self._llm_caller = LLMCaller[P]()
 
+    # TODO: remove this as there is no need to expose the LLM config
     def get_llm_config(self):
         return self._llm_config
 
-    async def execute(self, user_input: AgentInput, context: ConversationContext) -> AgentOutput:
-        agent_start_time = time.time()
-        llm_stats_list: list[LLMStats] = []
-        if user_input.message == "":
-            # If the user input is empty, set it to "(silence)"
-            # This is to avoid the agent failing to respond to an empty input
-            user_input.message = "(silence)"
-        msg = user_input.message.strip()  # Remove leading and trailing whitespaces
-        success = False
-        attempt_count = 0
-        model_response: ModelResponse | None = None
-        while not success and attempt_count < _MAX_ATTEMPTS:
-            attempt_count += 1
-            llm_start_time = time.time()
-            llm_response = await self._llm.generate_content(
-                    llm_input=ConversationHistoryFormatter.format_for_agent_generative_prompt(context, msg)
-            )
-            llm_end_time = time.time()
-            llm_stats = LLMStats(prompt_token_count=llm_response.prompt_token_count,
-                                 response_token_count=llm_response.response_token_count,
-                                 response_time_in_sec=round(llm_end_time - llm_start_time, 2))
-            response_text = llm_response.text
-            try:
-                model_response = extract_json(response_text, ModelResponse)
-                success = True
-            except ExtractJSONError:
-                log_message = (f"Attempt {attempt_count} failed to extract JSON "
-                               f"from conversation content: '{response_text}")
-                llm_stats.error = log_message
-                if attempt_count == _MAX_ATTEMPTS:
-                    # The agent failed to respond with a JSON object after the last attempt,
-                    self._logger.error(log_message)
-                    # And set the response to the model output and hope that the conversation can continue
-                    model_response = ModelResponse(message=response_text, finished=False,
-                                                   reasoning="Failed to respond with JSON")
-                else:
-                    self._logger.warning(log_message)
-            # Any other exception should be caught and logged
-            except Exception as e:  # pylint: disable=broad-except
-                self._logger.error("An error occurred while requesting a response from the model: %s",
-                                   e, exc_info=True)
-                llm_stats.error = str(e)
-            finally:
-                llm_stats_list.append(llm_stats)
-
-        # If it was not possible to get a model response, set the response to a default message
-        if model_response is None:
-            model_response = ModelResponse(
-                    reasoning="Failed to get a response",
-                    message="I am facing some difficulties right now, could you please repeat what you said?",
-                    finished=False)
-
-        self._logger.debug("Model input: %s", user_input.message)
-        self._logger.debug("Model output: %s", model_response)
-        agent_end_time = time.time()
-        response = AgentOutput(message_for_user=model_response.message,
-                               finished=model_response.finished,
-                               reasoning=model_response.reasoning,
-                               agent_type=self._agent_type,
-                               agent_response_time_in_sec=round(agent_end_time - agent_start_time, 2),
-                               llm_stats=llm_stats_list)
-        return response
-
-
-class _SimpleStatelessChatLLMAgent(Agent):
-    """
-    This is a simple agent that uses the GeminiStatelessChatLLM to respond to the user input in a conversation.
-    It seems that it performs equally well as the SimpleLLMAgent, but has some internal overhead as it
-    uses an internal chat session to manage the conversation history in a similar way as thew ConversationMemoryManager.
-    The code is kept here for reference, but it is not used in the current implementation.
-    TODO: Remove this class in the future.
-    """
-
-    def __init__(self, *, agent_type: AgentType, system_instructions: str, config: LLMConfig = LLMConfig()):
-        self._agent_type = agent_type
-        self._llm = GeminiStatelessChatLLM(system_instructions=system_instructions, config=config)
-        self._logger = logging.getLogger(self.__class__.__name__)
-
-    async def execute(self, user_input: AgentInput, context: ConversationContext) -> AgentOutput:
-        if user_input.message == "":
-            # If the user input is empty, set it to "(silence)"
-            # This is to avoid the agent failing to respond to an empty input
-            user_input.message = "(silence)"
-        msg = user_input.message.strip()  # Remove leading and trailing whitespaces
-        success = False
-        retry_count = 0
-        model_response: ModelResponse | None = None
-        while not success and retry_count < _MAX_ATTEMPTS:
-            retry_count += 1
-            llm_response = await self._llm.stateless_generate_content(
-                history=ConversationHistoryFormatter.format_history_for_agent_generative_prompt(context), llm_input=msg)
-            try:
-                model_response = extract_json(llm_response.text, ModelResponse)
-                success = True
-            except ExtractJSONError:
-                log_message = "Failed to extract JSON from conversation content '%s'"
-                if retry_count == 0:
-                    # If the agent failed to respond with a JSON object after the last retry,
-                    # log the error
-                    self._logger.error(log_message, llm_response.text)
-                else:
-                    self._logger.warning(log_message, llm_response.text)
-                    # If the agent failed to respond with a JSON object, set the response to the model output
-                    # and hope that the conversation can continue
-                    model_response = ModelResponse(message=str(llm_response.text), finished=False,
-                                                   reasoning="Failed to respond with JSON")
-
-        if model_response is None:
-            # If the model response is None, set the response to the model output
-            # and hope that the conversation can continue
-            model_response = ModelResponse(message=str("Model response is None"), finished=False,
-                                           reasoning="Failed to get a response")
-
-        self._logger.debug("Model input: %s", user_input.message)
-        self._logger.debug("Model output: %s", model_response)
-        response = AgentOutput(message_for_user=model_response.message,
-                               finished=model_response.finished,
-                               reasoning=model_response.reasoning,
-                               agent_type=self._agent_type)
-        return response
+    async def execute(self, user_input: AgentInput, context: ConversationContext) -> AgentOutput[P]:
+        return await self._llm_caller.call_llm_agent(
+            llm=self._llm,
+            agent_type=self._agent_type,
+            user_input=user_input,
+            context=context,
+            logger=self._logger)
