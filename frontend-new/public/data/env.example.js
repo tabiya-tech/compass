1eb26ba927ac362084424b60986f3fd9d76504c4
Bereket Terefe <berekterefe@gmail.com>
2024-07-03 11:03:11 +0300

    chore(frontend-new): document local deployment requirements (#132)

---
+++ frontend-new/public/data/env.example.js
@@ -0,0 +1,6 @@
+console.log('env.js loaded.....');
+window.tabiyaConfig = {
+  "FIREBASE_API_KEY": btoa("Some API Key"),
+  "FIREBASE_AUTH_DOMAIN": btoa("someAuthDomain"), // auth domain without the ".firebaseapp.com"
+  "BACKEND_URL": btoa("https://foo.bar/api")
+};
