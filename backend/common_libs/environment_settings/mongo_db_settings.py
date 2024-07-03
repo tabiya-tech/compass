2cca54388a7ff99ece4dbe87cebb27fccab5c2df
kingam <kingam@google.com>
2024-07-03 17:31:30 +0200

    fix(backend): Delete unused db settings.

--- backend/common_libs/environment_settings/mongo_db_settings.py
+++ backend/common_libs/environment_settings/mongo_db_settings.py
@@ -1,13 +1,4 @@
 from pydantic_settings import BaseSettings
-from pydantic import BaseModel
-
-
-class EmbeddingSettings(BaseModel):
-    """ Settings for the embedding models. """
-    skill_collection_name: str
-    occupation_collection_name: str
-    embedding_key: str
-    embedding_index: str
 
 
 class MongoDbSettings(BaseSettings):
