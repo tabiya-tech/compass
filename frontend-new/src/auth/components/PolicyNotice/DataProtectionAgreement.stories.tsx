da147cff242e4e0b3ec972797d72f8e3e247db52
Fides Noella Iragena <90243095+Fidesnoella@users.noreply.github.com>
2024-07-12 12:52:11 +0200

    chore(frontend-new): add DPA loading state (#148)

--- frontend-new/src/auth/components/PolicyNotice/DataProtectionAgreement.stories.tsx
+++ frontend-new/src/auth/components/PolicyNotice/DataProtectionAgreement.stories.tsx
@@ -1,14 +1,39 @@
 import { Meta, StoryObj } from "@storybook/react";
 import DataProtectionAgreement from "./DataProtectionPolicy";
+import { AuthContext, authContextDefaultValue } from "src/auth/AuthProvider";
 
 const meta: Meta<typeof DataProtectionAgreement> = {
   title: "Auth/DataProtectionAgreement",
   tags: ["autodocs"],
   component: DataProtectionAgreement,
+  decorators: [
+    (Story) => {
+      const mockAuthContextValue = {
+        ...authContextDefaultValue,
+        user: {
+          id: "123",
+          name: "John Doe",
+          email: "johndoe@mail.com",
+        },
+      };
+
+      return (
+        <AuthContext.Provider value={mockAuthContextValue}>
+          <Story />
+        </AuthContext.Provider>
+      );
+    },
+  ],
 };
 
 export default meta;
 
 export const Shown: StoryObj<typeof DataProtectionAgreement> = {
   args: {},
+};
+
+export const AcceptingDPA: StoryObj<typeof DataProtectionAgreement> = {
+  args: {
+    isLoading: true,
+  },
 };
