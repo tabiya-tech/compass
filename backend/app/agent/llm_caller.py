3614298c792065fa508ff1ee5d31058a19be41f4
Benisis, Apostolos <apostolos@benisis.de>
2024-07-21 20:57:45 +0300

    fix(backend): Fix the "repetition trap". that happens often with prompts that have Chain Of Thought reasoning tasks.

--- backend/app/agent/llm_caller.py
+++ backend/app/agent/llm_caller.py
@@ -2,6 +2,7 @@ import logging
 import time
 from typing import Generic, TypeVar, Type, Tuple
 
+from google.cloud.aiplatform_v1 import GenerationConfig
 from pydantic import BaseModel
 
 from app.agent.agent_types import LLMStats
@@ -43,6 +44,10 @@ class LLMCaller(Generic[RESPONSE_T]):
         success = False
         attempt_count = 0
         model_response: RESPONSE_T | None = None
+        # This is a hack as we do not have access to the model config any more
+        generation_config: GenerationConfig = llm._model._generation_config._raw_generation_config
+        original_frequency_penalty = generation_config.frequency_penalty
+        original_temperature = generation_config.temperature  # usually for json we use 0.0
         while not success and attempt_count < _MAX_ATTEMPTS:
             attempt_count += 1
             llm_start_time = time.time()
@@ -67,6 +72,28 @@ class LLMCaller(Generic[RESPONSE_T]):
                     # And set the response to the model output and hope that the caller can handle it
                 else:
                     logger.warning(log_message)
+                    if llm_stats.response_token_count == generation_config.max_output_tokens:
+                        # Most-likely we run into a "repetition trap". This happens often with prompts that have Chain Of Thought reasoning tasks.
+                        # We will increase the frequency_penalty and the temperature and return the model to avoid repetition.
+                        # However, higher frequency_penalty might cause the model to penalize punctuation and json format characters,
+                        # and in combination with the higher the temperature will most likely result in an invalid json.
+                        # Therefore, there is no guarantee that the result is a valid json.
+                        # However, we must perform this process to get unstuck from the repetition trap as experiments have
+                        # shown that just rerunning the model will not solve the problem on its own, but changing the parameters will.
+                        generation_config.frequency_penalty += 0.1
+                        if generation_config.frequency_penalty > 1.0:
+                            generation_config.frequency_penalty = 1.0
+                        generation_config.temperature += 0.1
+                        if generation_config.temperature > 1.0:
+                            generation_config.temperature = 1.0
+
+                        logger.warning("The model reached the maximum number of tokens %s.\n"
+                                       "To escape the repetition trap, we increased the frequency_penalty to %s\n"
+                                       "To escape the repetition trap, we increased the temperature to %s",
+                                       generation_config.max_output_tokens,
+                                       generation_config.frequency_penalty,
+                                       generation_config.temperature)
+
             # Any other exception should be caught and logged
             except Exception as e:  # pylint: disable=broad-except
                 logger.error("An error occurred while requesting a response from the model: %s",
@@ -74,6 +101,11 @@ class LLMCaller(Generic[RESPONSE_T]):
                 llm_stats.error = str(e)
             finally:
                 llm_stats_list.append(llm_stats)
+
+        # Reset the frequency_penalty to the original value
+        generation_config.frequency_penalty = original_frequency_penalty
+        # Reset the temperature to the original value
+        generation_config.temperature = original_temperature
 
         logger.debug("Model input: %s", llm_input)
         logger.debug("Model output: %s", model_response)
