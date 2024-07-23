d0b10011d34bb8660ecf9565c072516388a6318c
Anselme Irumva <61510549+irumvanselme@users.noreply.github.com>
2024-07-23 08:35:13 +0200

    feat(poc): add poc adjustments. [pulumi up] (#166)
    - frontend buckets and url paths.
    - remove poc ui from swagger docs.

--- backend/app/poc/__init__.py
+++ backend/app/poc/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI, APIRouter
 
 from app.poc.poc_routes import add_poc_route_endpoints
@@ -5,8 +7,14 @@ from app.users.auth import Authentication
 
 
 def add_poc_routes(app: FastAPI, authentication: Authentication):
+    target_env = os.getenv("TARGET_ENVIRONMENT")
+    include_in_schema = False
 
-    poc_router = APIRouter(prefix="/poc")
+    # Only include poc routes in the schema if the target environment is local
+    if target_env == "local":
+        include_in_schema = True
+
+    poc_router = APIRouter(prefix="/poc", include_in_schema=include_in_schema)
 
     ############################################
     # Add the poc routes
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    basePath: "/poc-ui",
     reactStrictMode: false,
 };
 
