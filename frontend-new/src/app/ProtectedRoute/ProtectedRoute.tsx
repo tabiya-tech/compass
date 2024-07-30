74716df64cc027f837c6790381041e37ae336f59
Bereket Terefe <berekterefe@gmail.com>
2024-07-30 12:27:53 +0300

    fix(frontend-new): register page reload issue fixed

--- frontend-new/src/app/ProtectedRoute/ProtectedRoute.tsx
+++ frontend-new/src/app/ProtectedRoute/ProtectedRoute.tsx
@@ -1,10 +1,8 @@
-import React, { useContext, useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
 import { AuthContext } from "src/auth/AuthProvider/AuthProvider";
 import { routerPaths } from "src/app/routerPaths";
 import { UserPreferencesContext } from "src/userPreferences/UserPreferencesProvider/UserPreferencesProvider";
-import { useSnackbar } from "src/theme/SnackbarProvider/SnackbarProvider";
-import { useAuthUser } from "src/auth/hooks/useAuthUser";
 
 interface ProtectedRouteProps {
   authenticationAndDPARequired: boolean;
@@ -12,22 +10,8 @@ interface ProtectedRouteProps {
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ authenticationAndDPARequired, children }) => {
-  const { updateUser } = useAuthUser();
-  const { user, handlePageLoad } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const { userPreferences } = useContext(UserPreferencesContext);
-  const navigate = useNavigate();
-  const { enqueueSnackbar } = useSnackbar();
-
-  useEffect(() => {
-    handlePageLoad(
-      (_user) => {
-        updateUser(_user);
-      },
-      (error) => {
-        // do nothing
-      }
-    );
-  }, [enqueueSnackbar, handlePageLoad, navigate, userPreferences?.accepted_tc, updateUser]);
 
   if (authenticationAndDPARequired && (!user || !userPreferences?.accepted_tc)) {
     // if the user is not found or the terms and conditions aren't accepted
@@ -36,7 +20,7 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ authenticationAndDPAReq
     return <Navigate to={routerPaths.LOGIN} replace />;
   }
 
-  if (!authenticationAndDPARequired && (user && userPreferences?.accepted_tc)) {
+  if (!authenticationAndDPARequired && user && userPreferences?.accepted_tc) {
     // if there is a user and they have accepted the terms and conditions
     // redirect to the root page
     return <Navigate to={routerPaths.ROOT} replace />;
