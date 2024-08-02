5de36c91d36a9a5db8a48ea6109b834cc0c2d9b5
Francesco Preta <fp627@nyu.edu>
2024-08-02 10:35:02 +0200

    cleaned code

--- backend/evaluation_tests/conversation_libs/utils.py
+++ backend/evaluation_tests/conversation_libs/utils.py
@@ -1,9 +1,13 @@
 import textwrap
 
 from app.agent.agent_types import AgentInput, AgentOutput
-from app.conversation_memory.conversation_memory_types import ConversationContext, ConversationTurn
+from app.conversation_memory.conversation_memory_types import (
+    ConversationContext,
+    ConversationTurn,
+)
 
-EVALUATION_OUTRO_PROMPT = textwrap.dedent("""            
+EVALUATION_OUTRO_PROMPT = textwrap.dedent(
+    """            
             Rate it from 0 to 5, 0 being worst 5 being best.
                     
             Respond only using a valid JSON format with the following fields:
@@ -11,24 +15,30 @@ EVALUATION_OUTRO_PROMPT = textwrap.dedent("""
                         able to keep focus. Overall, anything larger or equal to 3
                         indicates that the conversation was mostly focused.
             - "reason": a reason for the score.
-            """)
+            """
+)
 
-def _add_turn_to_context(user_input: str, agent_output: str, context: ConversationContext):
+
+def _add_turn_to_context(
+    user_input: str, agent_output: str, context: ConversationContext
+):
     turn: ConversationTurn = ConversationTurn(
         index=len(context.history.turns),
         input=AgentInput(message=user_input),
-        output=AgentOutput(message_for_user=agent_output,
-                           finished=False,
-                           agent_response_time_in_sec=0.0,
-                           llm_stats=[]
-                           )
+        output=AgentOutput(
+            message_for_user=agent_output,
+            finished=False,
+            agent_response_time_in_sec=0.0,
+            llm_stats=[],
+        ),
     )
     context.history.turns.append(turn)
     context.all_history.turns.append(turn)
 
+
 def _unwrap_all_history(context: ConversationContext) -> str:
     all_history_string = ""
     for turn in context.all_history.turns:
-        all_history_string +=f"User: {turn.input}\n"
-        all_history_string +=f"Agent: {turn.output}\n"
+        all_history_string += f"User: {turn.input}\n"
+        all_history_string += f"Agent: {turn.output}\n"
     return all_history_string
