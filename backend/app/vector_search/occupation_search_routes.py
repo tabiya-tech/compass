77c5a69605dab8cdecc500eeee8249697433ce23
Apostolos Benisis <apostolos.benisis@tabiya.org>
2024-08-14 13:42:56 +0300

    chore(backend): Correct inaccurate use of ids in the Esco Search Services. Add /poc/search/occupations-skills test route

--- backend/app/vector_search/occupation_search_routes.py
+++ backend/app/vector_search/occupation_search_routes.py
@@ -4,9 +4,9 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, Query
 from pydantic.main import BaseModel
 
-from app.vector_search.esco_entities import OccupationEntity
+from app.vector_search.esco_entities import OccupationEntity, OccupationSkillEntity
 from app.vector_search.similarity_search_service import SimilaritySearchService
-from app.vector_search.vector_search_dependencies import get_occupation_search_service
+from app.vector_search.vector_search_dependencies import get_occupation_search_service, get_occupation_skill_search_service
 
 logger = logging.getLogger(__name__)
 
@@ -34,6 +34,30 @@ def add_occupation_search_routes(app: APIRouter) -> None:
         try:
             occupations = await occupation_service.search(query=query, k=top_k)
             return OccupationsResponse(occupations=occupations)
+        except Exception as e:
+            logger.exception(e)
+            return {"error": str(e)}
+
+    class OccupationsSkillsResponse(BaseModel):
+        """
+        The response model for the occupations search endpoint.
+        """
+        occupations_skills: list[OccupationSkillEntity]
+
+    @app.get("/search/occupations-skills",
+             response_model=OccupationsSkillsResponse,
+             description="""
+             Semantically search for occupations based on a query. 
+             The search is based on the embeddings of the occupations, and uses
+             the cosine similarity to find the most similar occupations.""",
+             )
+    async def _search_occupations(
+            query: Annotated[str, Query(max_length=3000, description="The text to search for matching occupations")],
+            top_k: Annotated[int, Query(ge=1, le=100, description="The number of occupations to return")] = 5,
+            occupation_skill_service: SimilaritySearchService[OccupationSkillEntity] = Depends(get_occupation_skill_search_service)):
+        try:
+            occupations_skills = await occupation_skill_service.search(query=query, k=top_k)
+            return OccupationsSkillsResponse(occupations_skills=occupations_skills)
         except Exception as e:
             logger.exception(e)
             return {"error": str(e)}
