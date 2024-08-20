4d019a3fe4cfbb4bd0b5790c5bcb258276fd0864
Fides <fniragena@gmail.com>
2024-08-20 23:30:50 +0200

    chore(frontend-new): display the description for each skill in the Experiences drawer.

--- frontend-new/src/Experiences/components/ExperiencesDrawerContent/ExperiencesDrawerContent.test.tsx
+++ frontend-new/src/Experiences/components/ExperiencesDrawerContent/ExperiencesDrawerContent.test.tsx
@@ -7,6 +7,7 @@ import ExperiencesDrawerContent, {
 } from "src/Experiences/components/ExperiencesDrawerContent/ExperiencesDrawerContent";
 import { render, screen } from "src/_test_utilities/test-utils";
 import { mockExperiences } from "src/Experiences/ExperienceService/_test_utilities/mockExperiencesResponses";
+import { fireEvent, waitFor } from "@testing-library/react";
 
 describe("ReportDrawerContent", () => {
   test("should render ExperiencesDrawerContent correctly", () => {
@@ -28,6 +29,11 @@ describe("ReportDrawerContent", () => {
     expect(screen.getByTestId(DATA_TEST_ID.EXPERIENCES_DRAWER_CONTENT_OCCUPATION)).toBeInTheDocument();
     // AND the report drawer content skills to be in the document
     expect(screen.getByTestId(DATA_TEST_ID.EXPERIENCES_DRAWER_CONTENT_SKILLS)).toBeInTheDocument();
+    // AND the report drawer skills container to be in the document
+    expect(screen.getByTestId(DATA_TEST_ID.EXPERIENCES_DRAWER_SKILLS_CONTAINER)).toBeInTheDocument();
+    // AND the report drawer chips to be in the document
+    const chips = screen.getAllByTestId(DATA_TEST_ID.EXPERIENCES_DRAWER_CHIP);
+    chips.forEach((chip) => expect(chip).toBeInTheDocument());
     // AND to match the snapshot
     expect(reportDrawerContentContainer).toMatchSnapshot();
   });
@@ -62,5 +68,48 @@ describe("ReportDrawerContent", () => {
 
     // THEN expect No skills yet to be in the document
     expect(screen.getByText("No skills yet")).toBeInTheDocument();
+  });
+
+  test("should show skill description when chip is clicked", () => {
+    // GIVEN the ExperiencesDrawerContent component
+    const givenReportDrawerContent = <ExperiencesDrawerContent experience={{ ...mockExperiences[0] }} />;
+    // AND the component is rendered
+    render(givenReportDrawerContent);
+
+    // WHEN the chip is clicked
+    const chip = screen.getAllByTestId(DATA_TEST_ID.EXPERIENCES_DRAWER_CHIP)[0];
+    fireEvent.click(chip);
+
+    // THEN expect no errors or warning to have occurred
+    expect(console.error).not.toHaveBeenCalled();
+    expect(console.warn).not.toHaveBeenCalled();
+    // AND the popover to be in the document
+    const popoverEl = screen.getByTestId(DATA_TEST_ID.EXPERIENCES_DRAWER_POPOVER);
+    expect(popoverEl).toBeInTheDocument();
+    // AND the skill description to be in the document
+    expect(screen.getByText(mockExperiences[0].top_skills[0].description)).toBeInTheDocument();
+  });
+
+  test("should close popover when clicked outside", async () => {
+    // GIVEN the ExperiencesDrawerContent component
+    const givenReportDrawerContent = <ExperiencesDrawerContent experience={{ ...mockExperiences[0] }} />;
+    // AND the component is rendered
+    render(givenReportDrawerContent);
+    // AND the chip is clicked
+    const chip = screen.getAllByTestId(DATA_TEST_ID.EXPERIENCES_DRAWER_CHIP)[0];
+    fireEvent.click(chip);
+    // AND the popover is open
+    const popoverEl = screen.getByTestId(DATA_TEST_ID.EXPERIENCES_DRAWER_POPOVER);
+    expect(popoverEl).toBeVisible();
+
+    // WHEN the user clicks outside the popover
+    fireEvent.keyDown(popoverEl, { key: "Escape" });
+
+    // THEN expect the popover to be invisible
+    await waitFor(() => {
+      expect(popoverEl).not.toBeInTheDocument();
+    });
+    // AND expect the skill description to not be in the document
+    expect(screen.queryByText(mockExperiences[0].top_skills[0].description)).not.toBeInTheDocument();
   });
 });
--- frontend-new/src/Experiences/components/ExperiencesDrawerContent/ExperiencesDrawerContent.tsx
+++ frontend-new/src/Experiences/components/ExperiencesDrawerContent/ExperiencesDrawerContent.tsx
@@ -1,8 +1,9 @@
 import React, { useMemo } from "react";
-import { Box, Grid, Skeleton, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Box, Chip, Grid, Popover, Skeleton, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { Experience } from "src/Experiences/ExperienceService/Experiences.types";
 import { Theme } from "@mui/material/styles";
-
+import HelpTip from "src/theme/HelpTip/HelpTip";
+import InfoIcon from "@mui/icons-material/Info";
 const uniqueId = "34a59a9e-e7f6-4a10-8b72-0fd401c727de";
 
 export const DATA_TEST_ID = {
@@ -11,20 +12,41 @@ export const DATA_TEST_ID = {
   EXPERIENCES_DRAWER_CONTENT_DATE: `experiences-drawer-content-date-${uniqueId}`,
   EXPERIENCES_DRAWER_CONTENT_OCCUPATION: `experiences-drawer-content-occupation-${uniqueId}`,
   EXPERIENCES_DRAWER_CONTENT_SKILLS: `experiences-drawer-content-skills-${uniqueId}`,
+  EXPERIENCES_DRAWER_SKILLS_CONTAINER: `experiences-drawer-skills-container-${uniqueId}`,
+  EXPERIENCES_DRAWER_CHIP: `experiences-drawer-chip-${uniqueId}`,
+  EXPERIENCES_DRAWER_POPOVER: `experiences-drawer-popover-${uniqueId}`,
 };
 
 interface ExperienceProps {
   experience: Experience;
 }
 
+const capitalizeFirstLetter = (string: string): string => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
 const ExperiencesDrawerContent: React.FC<ExperienceProps> = ({ experience }) => {
   const theme = useTheme();
   const isSmallMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"));
+  const [skillDescription, setSkillDescription] = React.useState<string>("");
+  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+  const [isOpen, setIsOpen] = React.useState(false);
 
   const formattedSkills = useMemo(() => {
-    if (experience.top_skills.length === 0) return "No skills yet";
-    return experience.top_skills.map((skill) => skill.preferredLabel).join(", ");
+    if (experience.top_skills.length === 0) return [];
+    return experience.top_skills.map((skill) => skill);
   }, [experience.top_skills]);
+
+  const handleChipClick = (event: React.MouseEvent<HTMLElement>, description: string) => {
+    setAnchorEl(event.currentTarget);
+    setSkillDescription(description);
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setIsOpen(false);
+  };
 
   return (
     <Box
@@ -36,18 +58,59 @@ const ExperiencesDrawerContent: React.FC<ExperienceProps> = ({ experience }) => 
       <Typography variant="body1" fontWeight="bold" data-testid={DATA_TEST_ID.EXPERIENCES_DRAWER_CONTENT_OCCUPATION}>
         {experience.experience_title}
       </Typography>
-      <Typography
-        variant="body1"
-        sx={{ wordBreak: "break-all" }}
-        data-testid={DATA_TEST_ID.EXPERIENCES_DRAWER_CONTENT_SKILLS}
-      >
-        <b>Top Skills:</b> {formattedSkills}
-      </Typography>
       <Typography variant="body2" data-testid={DATA_TEST_ID.EXPERIENCES_DRAWER_CONTENT_DATE}>
         {experience.end_date && experience.start_date
           ? `${experience.start_date} — ${experience.end_date}`
           : experience.start_date || experience.end_date}
       </Typography>
+      <Box display="flex" alignItems="center">
+        <Typography
+          variant="body1"
+          sx={{ wordBreak: "break-all" }}
+          data-testid={DATA_TEST_ID.EXPERIENCES_DRAWER_CONTENT_SKILLS}
+        >
+          <b>Top Skills</b>
+        </Typography>
+        <HelpTip icon={<InfoIcon sx={{ padding: 0.1 }} />}>Tap on the skill to see more details</HelpTip>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        flexWrap="wrap"
+        gap={2}
+        data-testid={DATA_TEST_ID.EXPERIENCES_DRAWER_SKILLS_CONTAINER}
+      >
+        {formattedSkills.length === 0 ? (
+          <Typography>No skills yet</Typography>
+        ) : (
+          formattedSkills.map((skill) => (
+            <Chip
+              key={skill.UUID}
+              label={capitalizeFirstLetter(skill.preferredLabel)}
+              sx={{ color: theme.palette.text.secondary }}
+              onClick={(event) => handleChipClick(event, skill.description)}
+              data-testid={DATA_TEST_ID.EXPERIENCES_DRAWER_CHIP}
+            />
+          ))
+        )}
+      </Box>
+      <Popover
+        open={isOpen}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        onClose={handleClose}
+        data-testid={DATA_TEST_ID.EXPERIENCES_DRAWER_POPOVER}
+      >
+        <Typography sx={{ maxWidth: 500, padding: isSmallMobile ? 4 : 2 }}>{skillDescription}</Typography>
+      </Popover>
     </Box>
   );
 };
