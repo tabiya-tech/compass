ee52228b085502eb8deeee5fec46f909d06c0226
FPreta <frpreta@yahoo.it>
2024-11-28 10:03:52 +0100

    added tests for icatus skills

--- backend/evaluation_tests/linking_and_ranking_pipeline/experience_pipeline_test.py
+++ backend/evaluation_tests/linking_and_ranking_pipeline/experience_pipeline_test.py
@@ -124,17 +124,171 @@ test_cases = [
                              'maintain incident reporting records',
                              'present reports']
     ),
-]
+    ExperiencePipelineTestCase(
+            name="Icatus I42_3",
+            given_experience_title="I fill my grandma’s taxes",
+            given_company_name="Home",
+            given_responsibilities=['Maintain accurate financial records for dependent adults.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['maintain statutory books']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I42_2",
+            given_experience_title="I give my uncle his medication ",
+            given_company_name="Home",
+            given_responsibilities=['Maintain a clean and sanitary environment to ensure my uncle\'s well-being.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['hygiene in a health care setting']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I42_5",
+            given_experience_title="I keep an eye on my Grandpa",
+            given_company_name="Home",
+            given_responsibilities=["Maintain Grandpa's laundry needs."],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['wash the laundry']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I34_2",
+            given_experience_title="I dry the laundry ",
+            given_company_name="Home",
+            given_responsibilities=['Inspect clothing to determine appropriate drying methods.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['evaluate garment quality']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I34_1",
+            given_experience_title="I clean the sheets ",
+            given_company_name="Home",
+            given_responsibilities=['Inspect garments for damage before washing.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['evaluate garment quality']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I34_4",
+            given_experience_title="I clean my shoes",
+            given_company_name="Home",
+            given_responsibilities=['Repair and restore damaged clothing and footwear to a presentable condition.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['manufacture wearing apparel products']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I32_1",
+            given_experience_title="I clean the windows ",
+            given_company_name="Home",
+            given_responsibilities=['Maintain an adequate supply of cleaning materials.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['maintain inventory of cleaning supplies']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I32_2",
+            given_experience_title="I take away the snow around the house",
+            given_company_name="Home",
+            given_responsibilities=['Efficiently manage time to complete outdoor cleaning tasks.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['manage time in landscaping']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I31_1",
+            given_experience_title="I make cookies for my daughter",
+            given_company_name="Home",
+            given_responsibilities=['Prepare meals and snacks while adhering to strict food safety standards.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['comply with food safety and hygiene']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I31_4",
+            given_experience_title="I put fruits in bocals to store them for the winter. ",
+            given_company_name="Home",
+            given_responsibilities=['Maintain food safety and hygiene standards when storing and preserving food.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['comply with food safety and hygiene']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I35_1",
+            given_experience_title="I pay my bills online. ",
+            given_company_name="Home",
+            given_responsibilities=['Track and reconcile financial transactions to ensure timely bill payments.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['trace financial transactions']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I37_2",
+            given_experience_title="I look for a hairdresser for my mom and pay for an appointment.",
+            given_company_name="Home",
+            given_responsibilities=['Negotiate pricing and services with potential hairdressers to find the best value for your mother.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['sales argumentation']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I52_4",
+            given_experience_title="I help my church with accounting. ",
+            given_company_name="Home",
+            given_responsibilities=['Maintain accurate financial records in compliance with legal requirements.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['maintain statutory books']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I52_2",
+            given_experience_title="I sell cake at my dauther’s school baking parties",
+            given_company_name="Home",
+            given_responsibilities=['Maintain a consistent supply of cleaning materials for the kitchen.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['maintain inventory of cleaning supplies']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I52_1",
+            given_experience_title="I help clean the beach after a storm. ",
+            given_company_name="Home",
+            given_responsibilities=['Remove debris and debris-laden surfaces to restore the beach to a clean and safe condition.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['remove road surface']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I51_4",
+            given_experience_title="I check on my eldery neighboor when it’s very hot outside. ",
+            given_company_name="Home",
+            given_responsibilities=['Assist with laundry tasks to maintain a clean and comfortable environment.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['wash the laundry']
+        ),
+    ExperiencePipelineTestCase(
+            name="Icatus I51_5",
+            given_experience_title="I replace my uncle in his shop when he is away. ",
+            given_company_name="Home",
+            given_responsibilities=['Provide excellent customer service to ensure a positive shopping experience.'],
+            given_country_of_interest=Country.SOUTH_AFRICA,
+            given_work_type=WorkType.UNSEEN_UNPAID,
+            expected_top_skills=['guarantee customer satisfaction']
+        ),
+    ]
 
 
 @pytest.mark.asyncio
 @pytest.mark.evaluation_test
 @pytest.mark.parametrize("test_case", get_test_cases_to_run(test_cases), ids=[test_case.name for test_case in get_test_cases_to_run(test_cases)])
 async def test_experience_pipeline(test_case: ExperiencePipelineTestCase, get_search_services):
+    search_services = await get_search_services
     # When the skill linking tool is called with the given occupation and responsibilities
     experience_pipeline = ExperiencePipeline(
         config=ExperiencePipelineConfig(),
-        search_services=get_search_services
+        search_services=search_services
     )
     response = await experience_pipeline.execute(
         experience_title=test_case.given_experience_title,
@@ -145,4 +299,4 @@ async def test_experience_pipeline(test_case: ExperiencePipelineTestCase, get_se
     )
     # Then the expected top skills are returned
     actual_top_skill_preferred_labels = [skill.preferredLabel for skill in response.top_skills]
-    assert sorted(actual_top_skill_preferred_labels) == sorted(test_case.expected_top_skills)
+    assert set(test_case.expected_top_skills).issubset(set(actual_top_skill_preferred_labels))
