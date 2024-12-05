972f4d52b3ccdf662ed1a428646cab74ba1d6324
Bereket Terefe <bereket.terefe@tabiya.org>
2024-12-05 12:34:48 +0300

    fix(frontend) - configure sentry sourcemaps properly - make sure class names aren't minified, so that sentry issues dont become harder to understand [pulumi up]

--- frontend-new/src/auth/auth.error.types.ts
+++ frontend-new/src/auth/auth.error.types.ts
@@ -1,10 +1,14 @@
+enum AuthError {
+  TOKEN_ERROR = 'TokenError',
+}
+
 /**
  * Used for errors related to token generation, decoding and verification
  * */
 export class TokenError extends Error {
   constructor(message: string, cause: Error | string) {
     super(message);
-    this.name = TokenError.name;
+    this.name = AuthError.TOKEN_ERROR;
     this.cause = cause;
   }
 }
