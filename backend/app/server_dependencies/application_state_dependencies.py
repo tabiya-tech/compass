a06c1ff45a1ce53cf3ca86e97b8afedff3a15345
Benisis, Apostolos <apostolos.benisis@tabiya.org>
2024-12-10 11:41:02 +0100

    fix(backend): get_application_state_manager should use the asyncio instead of the threading Lock

--- backend/app/server_dependencies/application_state_dependencies.py
+++ backend/app/server_dependencies/application_state_dependencies.py
@@ -1,4 +1,4 @@
-from threading import Lock
+import asyncio
 
 from fastapi.params import Depends
 from motor.motor_asyncio import AsyncIOMotorDatabase
@@ -8,14 +8,14 @@ from app.server_dependencies.db_dependencies import CompassDBProvider
 from app.store.database_application_state_store import DatabaseApplicationStateStore
 
 # Lock to ensure that the singleton instance is thread-safe
-_application_state_manager_lock = Lock()
+_application_state_manager_lock = asyncio.Lock()
 _application_state_manager_singleton: ApplicationStateManager | None = None
 
 
 async def get_application_state_manager(db: AsyncIOMotorDatabase = Depends(CompassDBProvider.get_application_db)) -> ApplicationStateManager:
     global _application_state_manager_singleton
     if _application_state_manager_singleton is None:  # initial check to avoid the lock if the singleton instance is already created (lock is expensive)
-        with _application_state_manager_lock:  # before modifying the singleton instance, acquire the lock
+        async with _application_state_manager_lock:  # before modifying the singleton instance, acquire the lock
             if _application_state_manager_singleton is None:  # double check after acquiring the lock
                 _application_state_manager_singleton = ApplicationStateManager(DatabaseApplicationStateStore(db))
     return _application_state_manager_singleton
