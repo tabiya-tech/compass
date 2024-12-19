a14a4933f44b93e484e026582cc9e45249836def
Fides <noella.fides@tabiya.org>
2024-12-19 13:45:35 +0200

    chore: add reaction reason popover

--- frontend-new/src/feedback/reaction/reaction.types.ts
+++ frontend-new/src/feedback/reaction/reaction.types.ts
@@ -8,19 +8,27 @@ export enum ReactionReason {
   BIASED = "biased",
   INCORRECT = "incorrect",
   WRONG_PHRASE = "wrong_phrasing",
-  WRONG_LANGUAGE = "wrong_language"
+  WRONG_LANGUAGE = "wrong_language",
 }
+
+export const ReactReasonMessages = {
+  [ReactionReason.OFFENSIVE]: "Offensive",
+  [ReactionReason.BIASED]: "Biased",
+  [ReactionReason.INCORRECT]: "Incorrect",
+  [ReactionReason.WRONG_PHRASE]: "Wrong Phrasing",
+  [ReactionReason.WRONG_LANGUAGE]: "Wrong Language",
+};
 
 export class Reaction {
   constructor(
     public kind: ReactionType,
-    public reason?: ReactionReason,
+    public reason?: ReactionReason
   ) {
     if (kind === ReactionType.LIKE && reason) {
-      throw new Error('Like reactions cannot have a reason');
+      throw new Error("Like reactions cannot have a reason");
     }
     if (kind === ReactionType.DISLIKE && !reason) {
-      throw new Error('Dislike reactions must have a reason');
+      throw new Error("Dislike reactions must have a reason");
     }
   }
 }
