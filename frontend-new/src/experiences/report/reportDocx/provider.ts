520b7ee3fb489b6416ad6f74804b7815c9a1d3a1
Apostolos Benisis <apostolos.benisis@tabiya.org>
2025-01-24 16:51:16 +0100

    fix(frontend): handle exceptions when generating the skills reports (PDF, DOCX) [pulumi up]

--- frontend-new/src/experiences/report/reportDocx/provider.ts
+++ frontend-new/src/experiences/report/reportDocx/provider.ts
@@ -4,15 +4,19 @@ import { saveAs } from "src/experiences/saveAs";
 
 export class DocxReportDownloadProvider implements IReportFormatProvider {
   async download(props: ReportProps) {
-    const fileName = "compass-skills-report.docx";
-    const blob = await SkillReportDocx({
-      name: props.name,
-      email: props.email,
-      phone: props.phone,
-      address: props.address,
-      experiences: props.experiences,
-      conversationConductedAt: props.conversationConductedAt,
-    });
-    saveAs(blob, fileName);
+    try {
+      const fileName = "compass-skills-report.docx";
+      const blob = await SkillReportDocx({
+        name: props.name,
+        email: props.email,
+        phone: props.phone,
+        address: props.address,
+        experiences: props.experiences,
+        conversationConductedAt: props.conversationConductedAt,
+      });
+      saveAs(blob, fileName);
+    } catch (error) {
+      console.error("Failed to download report", error);
+    }
   }
 }
