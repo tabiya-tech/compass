e249cb0935aa6020b6bba573d0cbe1a03576181b
Fides <noella.fides@tabiya.org>
2025-03-13 14:25:27 +0200

    chore(frontend): log invalid invitation codes as errors and include the failing invitation code for sentry tracking

--- frontend-new/src/error/restAPIError/logger.ts
+++ frontend-new/src/error/restAPIError/logger.ts
@@ -3,15 +3,19 @@
 import { RestAPIError } from "src/error/restAPIError/RestAPIError";
 
 export function writeRestAPIErrorToLog(err: RestAPIError, logFunction: (msg: any) => void): void {
-  const logMessage = `ServiceError: ${err.serviceName} ${err.serviceFunction} ${err.errorCode} ${err.method} ${err.path} ${err.statusCode}`;
+  const logMessage = `ServiceError: ${err.message}, `;
   const obj = {};
   for (const propertyName in err) {
     if (typeof err[propertyName] !== "function") {
       obj[propertyName] = err[propertyName];
     }
   }
-  obj["message"] = err.message;
+  obj["serviceFunction"] = err.serviceFunction;
+  obj["serviceName"] = err.serviceName;
+  obj["method"] = err.method;
+  obj["errorCode"] = err.errorCode;
   obj["stack"] = err.stack;
   obj["class"] = err.name === "Error" ? err.constructor.name : err.name;
-  logFunction(logMessage, obj);
+
+  logFunction(logMessage, JSON.stringify(obj, null, 2));
 }
