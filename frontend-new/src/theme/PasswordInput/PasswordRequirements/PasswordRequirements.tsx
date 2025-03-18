6f1d89fe3645da6dc3471e3b7006d7d725bf046b
Bereket Terefe <bereket.terefe@tabiya.org>
2025-03-18 09:55:12 +0300

    fix(frontend): each child in a list should have a unique key prop warning COM-495

--- frontend-new/src/theme/PasswordInput/PasswordRequirements/PasswordRequirements.tsx
+++ frontend-new/src/theme/PasswordInput/PasswordRequirements/PasswordRequirements.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 const uniqueId = "480b210a-d548-4fc6-a2c0-35f46e98e537";
@@ -20,7 +20,7 @@ interface PasswordRequirementsProps {
 const PasswordRequirements: React.FC<PasswordRequirementsProps> = ({ validationResults }) => {
   const theme = useTheme();
 
-  const validationCriteria = [
+  const validationCriteria = useMemo(() => [
     { label: "Password must be at least 8 characters long.", isValid: validationResults.isLongEnough },
     { label: "Password must include at least one lowercase letter.", isValid: validationResults.hasLowercase },
     { label: "Password must include at least one uppercase letter.", isValid: validationResults.hasUppercase },
@@ -29,16 +29,15 @@ const PasswordRequirements: React.FC<PasswordRequirementsProps> = ({ validationR
       label: "Password must include at least one special character such as: !@#$%*& etc.",
       isValid: validationResults.hasSpecialChar,
     },
-  ];
+  ], [validationResults]);
 
   return (
     // we have to use a component that can be a child of a <p>
     // since this is going to be used in the helperText field of an mui TextField component
-    <p data-testid={DATA_TEST_ID.PASSWORD_REQUIREMENTS}>
+    <span data-testid={DATA_TEST_ID.PASSWORD_REQUIREMENTS}>
       {validationCriteria.map((criteria, index) => (
-        <>
+        <span key={index}>
           <Typography
-            key={index}
             variant="caption"
             color={criteria.isValid ? theme.palette.success.dark : theme.palette.error.main}
           >
@@ -46,9 +45,9 @@ const PasswordRequirements: React.FC<PasswordRequirementsProps> = ({ validationR
           </Typography>
           <br />
           {/* we need a <br /> to arrange the password requirements in a column */}
-        </>
+        </span>
       ))}
-    </p>
+    </span>
   );
 };
 
