10f75dda5f196d84a986a2ad6487fd351d35830b
Anselme Irumva <anselme.irumva@tabiya.org>
2025-03-25 11:09:20 +0200

    fix(tests): event types tests should test default values from app config

--- backend/common_libs/test_utilities/random_data.py
+++ backend/common_libs/test_utilities/random_data.py
@@ -5,6 +5,9 @@ This module provides a function to generate random data
 import string
 import random
 
+from app.app_config import ApplicationConfig
+from app.version.types import Version
+
 
 def get_random_session_id():
     return random.randint(0, (1 << 48) - 1)  # nosec B311 # random number for a test session
@@ -40,3 +43,27 @@ def get_random_base64_string(length: int):
     """
 
     return ''.join(random.choices(string.ascii_letters + string.digits + '+/', k=length))
+
+
+def get_random_version() -> Version:
+    """
+    Generate a random version instance
+    """
+    return Version(
+        branch=get_random_printable_string(10),
+        sha="".join(random.choices(string.ascii_letters + string.digits, k=40)),
+        date="".join(random.choices(string.ascii_letters + string.digits, k=40)),
+        buildNumber=str(random.randint(0, 1000))
+    )
+
+
+def get_random_application_config() -> ApplicationConfig:
+    """
+    Generate a random application config instance
+    """
+
+    return ApplicationConfig(
+        version_info=get_random_version(),
+        environment_name=get_random_printable_string(10),
+        enable_metrics=random.choice([True, False]),
+    )
