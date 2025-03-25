6d2b89b91484aafe2ed037480dc479c64c03cb1f
Fides <noella.fides@tabiya.org>
2025-03-25 11:00:30 +0200

    chore(frontend): refine overall feedback form spacing

---
+++ frontend-new/src/feedback/overallFeedback/feedbackForm/useIsSmallOrShortScreen.ts
@@ -0,0 +1,15 @@
+import { useMediaQuery, useTheme } from "@mui/material";
+
+export function useIsSmallOrShortScreen() {
+  const theme = useTheme()
+
+  const isSmallMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  // Material UI breakpoints are based on width. Since we want to control our small screens based on heights and adjust
+  // some spacings and paddings, we need to know if the screen is short.
+  // We want to do this on feedback form as it doesn't usually looks good on small screens in terms of height.
+  // 600px is the height of the small mobile screen(sm)
+  const isShortScreen = useMediaQuery("(max-height: 600px)");
+
+  return isSmallMobile || isShortScreen;
+}
