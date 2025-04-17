ad530ff182493a933d82d9a06ad87fdec528828c
Benisis, Apostolos <apostolos.benisis@tabiya.org>
2025-04-17 15:52:38 +0200

    fix(backend/llm): add new line instead of <br> in the first message of the skills explorer agent. Fix the broken tests

--- backend/evaluation_tests/skill_explorer_agent/_conversation_llm_test.py
+++ backend/evaluation_tests/skill_explorer_agent/_conversation_llm_test.py
@@ -6,28 +6,40 @@ import pytest
 from app.agent.agent_types import AgentInput, AgentOutput
 from app.conversation_memory.conversation_memory_manager import ConversationMemoryManager
 from app.conversation_memory.conversation_memory_types import ConversationMemoryManagerState, ConversationContext, ConversationTurn
+from app.countries import Country
 from app.server_config import UNSUMMARIZED_WINDOW_SIZE, TO_BE_SUMMARIZED_WINDOW_SIZE
 from app.agent.experience import WorkType
 from app.agent.skill_explorer_agent._conversation_llm import _ConversationLLM, _FINAL_MESSAGE
+from common_libs.test_utilities import get_random_session_id
 from evaluation_tests.compass_test_case import CompassTestCase
 from evaluation_tests.get_test_cases_to_run_func import get_test_cases_to_run
 
 
 class _TestCaseConversation(CompassTestCase):
     # The GIVEN
+    summary: str
+    """The summary of the conversation context until now."""
+
     turns: list[tuple[str, str]]
     """
-    The conversation history.
+    The conversation history between the user and the agent until now.
     First element is what the user said, second element is what the agent said.
     """
 
-    summary: str
-
     experiences_explored: list[str]
+    """
+    The experiences that have been explored so far.
+    """
+
+    country_of_user: Country
+    """
+    The country of the user.
+    """
 
 
 test_cases = [
     _TestCaseConversation(
+        country_of_user=Country.SOUTH_AFRICA,
         name="first_experience_to_explore",
         summary=("I told you I sell kotas for money and that I've never worked for anyone else. "
                  "I also told you that I help my brother out with his car, starting about 2 years ago when I got my driver's license. "
@@ -54,10 +66,12 @@ test_cases = [
         experiences_explored=[]
     ),
     _TestCaseConversation(
+        country_of_user=Country.SOUTH_AFRICA,
         name="german",
         summary=("Ich habe Ihnen gesagt, dass ich Kotas verkaufe, um Geld zu verdienen, und dass ich noch nie für jemand anderen gearbeitet habe. "
                  "Ich habe Ihnen auch gesagt, dass ich meinem Bruder mit seinem Auto helfe, seit etwa 2 Jahren, als ich meinen Führerschein bekommen habe. "
-                 "Sie haben mich gefragt, ob es einen bestimmten Ort gibt, an dem ich ihm helfe. Ich habe Ihnen gesagt, dass ich ihm überall helfe, wo er Unterstützung braucht, "
+                 "Sie haben mich gefragt, ob es einen bestimmten Ort gibt, an dem ich ihm helfe. "
+                 "Ich habe Ihnen gesagt, dass ich ihm überall helfe, wo er Unterstützung braucht, "
                  "manchmal bei ihm zu Hause, manchmal in der Werkstatt. Sie haben mich gefragt, ob ich ihm immer noch mit seinem Auto helfe."),
         turns=[("Ja, das tue ich immer noch.",
                 "Danke, also hilfst du deinem Bruder seit August 2022 mit seinem Auto, und du tust es immer noch. "
@@ -78,15 +92,33 @@ test_cases = [
         experiences_explored=[]
     ),
     _TestCaseConversation(
+        country_of_user=Country.SOUTH_AFRICA,
         name="second_experience",
-        summary="_SUMMARY_:\nI told you about my kota business and a typical day at work. I explained that I wake up early, prepare ingredients, drive my canteen to Joburg, set up, sell kotas, and then pack up and go home. You asked me to elaborate on other tasks I do at work. I told you that I also make the kotas, serve customers, take orders, handle money, and clean up. You acknowledged that I'm really involved in every aspect of my business and asked me what the most important things are that I need to do at work. I told you that making sure I have enough ingredients, finding a good spot to sell, and keeping my customers happy are the most important things. You asked me how I decide what task to do first each day. \n\nI usually start by preparing the ingredients, then I drive to my spot. I try to get there early so I can set up before the rush starts.",
+        summary="_SUMMARY_:\nI told you about my kota business and a typical day at work. "
+                "I explained that I wake up early, prepare ingredients, drive my canteen to Joburg, set up, sell kotas, "
+                "and then pack up and go home. You asked me to elaborate on other tasks I do at work. "
+                "I told you that I also make the kotas, serve customers, take orders, handle money, and clean up. "
+                "You acknowledged that I'm really involved in every aspect of my business "
+                "and asked me what the most important things are that I need to do at work. "
+                "I told you that making sure I have enough ingredients, finding a good spot to sell, "
+                "and keeping my customers happy are the most important things. "
+                "You asked me how I decide what task to do first each day. \n\n"
+                "I usually start by preparing the ingredients, then I drive to my spot. "
+                "I try to get there early so I can set up before the rush starts.",
         turns=[(
-            "I usually start with preparing the kotas, then I clean my canteen, and then I drive around to sell them. I try to make sure I have enough ingredients before I start selling",
+            "I usually start with preparing the kotas, then I clean my canteen, and then I drive around to sell them. "
+            "I try to make sure I have enough ingredients before I start selling",
             "That's a good system.  Are there any tasks that you specifically don't take care of? Which ones?"
         ), ("I don't handle marketing or advertising. I rely on word of mouth and my location to get customers.",
             'Thank you for sharing these details! I have all the information I need.'),
             ('ok what now?',
-             'After examining the information you provided, I identified the following skills for your experience "Selling Kotas":\n• administer ingredients in food production\n• unload supplies\n• customer service\n• maintain work area cleanliness\n• take payments for bills\n\n')
+             'After examining the information you provided, '
+             'I identified the following skills for your experience "Selling Kotas":\n'
+             '• administer ingredients in food production\n'
+             '• unload supplies\n'
+             '• customer service\n'
+             '• maintain work area cleanliness\n'
+             '• take payments for bills\n\n')
 
         ],
         experiences_explored=["Selling Kotas (Self-Employed), 2023/08 - Present, Joburg"]
@@ -97,10 +129,10 @@ test_cases = [
 @pytest.mark.evaluation_test
 @pytest.mark.parametrize('test_case', get_test_cases_to_run(test_cases),
                          ids=[case.name for case in get_test_cases_to_run(test_cases)])
-async def test_skills_explorer_agent_simulated_user(test_case):
+async def test_skills_explorer_agent_first_message(test_case):
     # The conversation manager for this test
     conversation_manager = ConversationMemoryManager(UNSUMMARIZED_WINDOW_SIZE, TO_BE_SUMMARIZED_WINDOW_SIZE)
-    conversation_manager.set_state(state=ConversationMemoryManagerState(1))
+    conversation_manager.set_state(state=ConversationMemoryManagerState(session_id=get_random_session_id()))
     context = await conversation_manager.get_conversation_context()
     # GIVEN the previous conversation context
     for turn in test_case.turns:
@@ -110,6 +142,7 @@ async def test_skills_explorer_agent_simulated_user(test_case):
     # WHEN the conversation LLM is executed
     llm = _ConversationLLM()
     out = await llm.execute(
+        country_of_user=test_case.country_of_user,
         experiences_explored=test_case.experiences_explored,
         first_time_for_experience=True,
         user_input=AgentInput(message="", is_artificial=True),
@@ -120,8 +153,12 @@ async def test_skills_explorer_agent_simulated_user(test_case):
     )
     logging.getLogger().info(out)
 
+    # THEN expect that the conversation has not ended
     assert out.message_for_user != _FINAL_MESSAGE
-    assert out.message_for_user.find("baker. \n\n") != -1
+    # AND the model is asking question about the currently explored experience
+    assert out.message_for_user.lower().find("baker") != -1
+    # And there are not <br> tags in the output
+    assert out.message_for_user.find("<br>") == -1
 
 
 def _add_turn_to_context(user_input: str, agent_output: str, context: ConversationContext):
