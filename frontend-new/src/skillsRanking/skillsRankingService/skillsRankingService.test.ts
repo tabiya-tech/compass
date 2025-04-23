f42488414ffc589b8c29251d3a4a5fa0f08e8128
Anselme Irumva <anselme.irumva@tabiya.org>
2025-04-23 07:12:16 +0200

    wip(frontend): add skills ranking frontend service

---
+++ frontend-new/src/skillsRanking/skillsRankingService/skillsRankingService.test.ts
@@ -0,0 +1,33 @@
+import * as EnvServiceModule from "src/envService";
+import { SkillsRankingService } from "./skillsRankingService";
+
+describe("SkillsRankingService", () => {
+  describe("SkillsRankingService", () => {
+    test.each([
+      [true, "true"],
+      [true, "TRUE"],
+      [true, "True"],
+      [true, "tRuE"],
+
+      [false, "false"],
+      [false, "FALSE"],
+      [false, "False"],
+      [false, "fAlSe"],
+      [false, ""],
+
+      [false, "bar"],
+    ])("should return '%s' for '%s'", (expected, input) => {
+      // GIVEN envService.getSkillsRankingEnabled is mocked to return input
+      const getSkillsRankingEnabledMock = jest.spyOn(EnvServiceModule, "getSkillsRankingEnabled").mockReturnValue(input);
+
+      // WHEN isSkillsRankingFeatureEnabled is called
+      const isSkillsRankingEnabled = SkillsRankingService.getInstance().isSkillsRankingFeatureEnabled();
+
+      // THEN envService.getSkillsRankingEnabled should be called
+      expect(getSkillsRankingEnabledMock).toHaveBeenCalled()
+
+      // AND the result should be as expected
+      expect(isSkillsRankingEnabled).toBe(expected);
+    });
+  });
+});
