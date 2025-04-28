e228083ff3994e7b5bade13edfeb198f8fa177c3
Bereket Terefe <bereket.terefe@tabiya.org>
2025-04-28 17:04:00 +0300

    feat(backend): attach relevant_experiments all AbstractUserAccountEvent metrics from the user preferences

--- backend/app/metrics/services/test_get_metrics_service.py
+++ backend/app/metrics/services/test_get_metrics_service.py
@@ -5,6 +5,7 @@ import pytest_mock
 
 from common_libs.test_utilities import get_random_application_config
 from app.metrics.services.get_metrics_service import get_metrics_service
+from app.users.repositories import IUserPreferenceRepository
 
 
 @pytest.mark.asyncio
@@ -20,10 +21,13 @@ class TestGetMetricsService:
         # AND given sample application config
         given_app_config = get_random_application_config()
 
+        # AND given a mock user preferences repository
+        _mock_user_preference_repository = mocker.MagicMock(spec=IUserPreferenceRepository)
+
         # WHEN get_metrics_service is called concurrently
         metrics_service_instance_1, metrics_service_instance_2 = await asyncio.gather(
-            get_metrics_service(application_db=_in_memory_metrics_database, app_config=given_app_config),
-            get_metrics_service(application_db=_in_memory_metrics_database, app_config=given_app_config)
+            get_metrics_service(application_db=_in_memory_metrics_database, app_config=given_app_config, user_preference_repository=_mock_user_preference_repository),
+            get_metrics_service(application_db=_in_memory_metrics_database, app_config=given_app_config, user_preference_repository=_mock_user_preference_repository)
         )
 
         # THEN the service should not be None and they should refer to the same instance.
@@ -39,6 +43,9 @@ class TestGetMetricsService:
         # AND the enable_metrics should be from application config
         assert metrics_service_instance_1.enable_metrics == given_app_config.enable_metrics  # type: ignore
 
+        # AND the user preferences repository should be set
+        assert metrics_service_instance_1._user_preference_repository == _mock_user_preference_repository  # type: ignore
+
     async def test_get_metrics_service_subsequent_calls(self, mocker: pytest_mock.MockFixture):
         # GIVEN random in-memory metrics database
         _in_memory_metrics_database = mocker.MagicMock()
@@ -46,12 +53,15 @@ class TestGetMetricsService:
         # AND given sample application config
         given_app_config = get_random_application_config()
 
+        # AND given a mock user preferences repository
+        _mock_user_preference_repository = mocker.MagicMock(spec=IUserPreferenceRepository)
+
         # WHEN get_metrics_service is called subsequently
         metrics_service_instance_1 = await get_metrics_service(
-            application_db=_in_memory_metrics_database, app_config=given_app_config)
+            application_db=_in_memory_metrics_database, app_config=given_app_config, user_preference_repository=_mock_user_preference_repository)
 
         metrics_service_instance_2 = await get_metrics_service(
-            application_db=_in_memory_metrics_database, app_config=given_app_config)
+            application_db=_in_memory_metrics_database, app_config=given_app_config, user_preference_repository=_mock_user_preference_repository)
 
         # THEN the service should not be None and they should refer to the same instance.
         assert metrics_service_instance_1 is not None
@@ -64,4 +74,7 @@ class TestGetMetricsService:
         assert metrics_service_instance_1._metrics_repository.db == _in_memory_metrics_database  # type: ignore
 
         # AND the enable_metrics should be from application config
-        assert metrics_service_instance_1.enable_metrics == given_app_config.enable_metrics   # type: ignore
+        assert metrics_service_instance_1.enable_metrics == given_app_config.enable_metrics  # type: ignore
+
+        # AND the user preferences repository should be set
+        assert metrics_service_instance_1._user_preference_repository == _mock_user_preference_repository  # type: ignore
