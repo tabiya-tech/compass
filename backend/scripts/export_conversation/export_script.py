7474b89a0946aa0495dec37b1d098f88cf971d38
Anselme Irumva <anselme.irumva@tabiya.org>
2025-05-07 17:30:02 +0200

    feat(be-scripts): improve export conversations script to support TXT format and export conversations report

--- backend/scripts/export_conversation/export_script.py
+++ backend/scripts/export_conversation/export_script.py
@@ -4,18 +4,19 @@ import argparse
 import asyncio
 import logging
 import os.path
+from datetime import datetime
 from textwrap import dedent
 from typing import Optional
 
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
-from app.application_state import ApplicationStateStore
-from common_libs.logging.log_utilities import setup_logging_config
-
-from constants import SCRIPT_DIR, DEFAULT_EXPORTS_DIR
-from _common import StoreType, create_store, get_db_connection
 from _application_state_queue import ApplicationStateQueue, _fetcher, _saver
+from _common import StoreType, create_store, get_db_connection
+from app.application_state import ApplicationStateStore
+from app.server_dependencies.database_collections import Collections
+from common_libs.logging.log_utilities import setup_logging_config
+from constants import SCRIPT_DIR, DEFAULT_EXPORTS_DIR
 
 # set up the logging
 setup_logging_config(os.path.join(SCRIPT_DIR, "logging.cfg.yaml"))
@@ -63,10 +64,19 @@ def _get_target_store(output_directory: str, target_type: StoreType) -> Applicat
         target_store = create_store(target_type, folder_path=output_directory)
     elif target_type == "MD":
         target_store = create_store(target_type, folder_path=output_directory)
+    elif target_type == "TXT":
+        target_store = create_store(target_type, folder_path=output_directory)
     else:
         raise ValueError(f"Unsupported target store type: {target_type}")
 
     return target_store
+
+
+def _valid_datetime(datetime_str: str):
+    try:
+        return datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"Invalid date: '{datetime_str}'. Expected format: YYYY-MM-DD HH:MM:SS")
 
 
 def _parse_args():
@@ -82,16 +92,17 @@ def _parse_args():
         Notes:
           - When exporting to JSON, files are saved in the 'output_dir/{{ session-id }}/state.json' directory
           - When exporting to MD, files are saved in the 'output_dir/{{ session-id }}/conversation.md' directory
+          - When exporting to TXT, files are saved in the 'output_dir/{{ date }}.txt' directory
           - The script will exit with code 1 if the export fails
         """)
     )
-    
+
     # Required session IDs
     parser.add_argument(
         "--session-ids",
         type=int,
         nargs="+",
-        required=True,
+        required=False,
         help="Session IDs to export, separated by space. eg: 1 2 3"
     )
 
@@ -107,9 +118,23 @@ def _parse_args():
     parser.add_argument(
         "--target",
         type=str,
-        choices=["JSON", "MD"],
+        choices=["JSON", "MD", "TXT"],
         required=True,
         help="Target store type (JSON or MD)"
+    )
+
+    parser.add_argument(
+        "--start-datetime",
+        type=_valid_datetime,
+        required=False,
+        help="The start date time to filter the conversations. Format: \"YYYY-MM-DD HH:MM:SS\", they should be provided in quotes because of the space"
+    )
+
+    parser.add_argument(
+        "--end-datetime",
+        type=_valid_datetime,
+        required=False,
+        help="The end date time to filter the conversations. Format: \"YYYY-MM-DD HH:MM:SS\" they should be provided in quotes because of the space"
     )
 
     parser.add_argument(
@@ -119,12 +144,16 @@ def _parse_args():
         help=f"The Output directory for th export script. Default value is {DEFAULT_EXPORTS_DIR}. "
              f"Absolute path or relative path to the running directory"
     )
-    
+
     args = parser.parse_args()
 
     # Prevent source type and target type from being the same.
     if args.source == args.target:
         parser.error("Source and target store types cannot be the same")
+
+    # Filter out not implemented combinations.
+    if (args.start_datetime or args.end_datetime) and len(args.session_ids) > 0:
+        parser.error("Cannot use both session IDs and date range filters at the same time")
 
     return args
 
@@ -134,7 +163,9 @@ async def export_conversations(
         source_type: StoreType,
         target_type: StoreType,
         output_directory: str,
-        queue_size: int = 5
+        queue_size: int = 5,
+        start_datetime: Optional[datetime] = None,
+        end_datetime: Optional[datetime] = None
 ) -> None:
     """
     Export multiple conversations from source to target store.
@@ -148,6 +179,8 @@ async def export_conversations(
         target_type: Type of target store ('JSON' or 'MD')
         output_directory: Directory where the exported files will be saved
         queue_size: Size of the processing queue.
+        start_datetime: Start date time to filter the conversations.
+        end_datetime: End date time to filter the conversations.
     """
 
     # get the script settings.
@@ -165,12 +198,29 @@ async def export_conversations(
     # 3. Create a queue for application states.
     queue: ApplicationStateQueue = asyncio.Queue(maxsize=queue_size)
 
+    target_session_ids = session_ids
+    if start_datetime and end_datetime and source_type == "DB":
+        # Filter session IDs based on the provided date range.
+        source_db = get_db_connection(settings.source_mongodb_uri, settings.source_database_name)
+
+        sessions = await (source_db.get_collection(Collections.AGENT_DIRECTOR_STATE)
+                          .find(dict(
+            conversation_conducted_at={
+                "$gte": start_datetime,
+                "$lt": end_datetime
+            }),
+            dict(
+                session_id=1
+            )).to_list(length=None))
+
+        target_session_ids = [s["session_id"] for s in sessions]
+
     # 4. Run both stages (fetch and save) concurrently.
     await asyncio.gather(
         _fetcher(
             queue=queue,
             source_store=source_store,
-            session_ids=session_ids),
+            session_ids=target_session_ids),
 
         _saver(
             queue=queue,
@@ -180,13 +230,15 @@ async def export_conversations(
 
 async def main():
     args = _parse_args()
-    
+
     try:
         await export_conversations(
             session_ids=args.session_ids,
             source_type=args.source,
             target_type=args.target,
             output_directory=args.output_dir,
+            start_datetime=args.start_datetime,
+            end_datetime=args.end_datetime,
         )
     except Exception as e:
         logger.error(f"Error during export: {str(e)}")
