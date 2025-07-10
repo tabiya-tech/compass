719677dc7f4a7eb8038106bb67d9a2e13ade4962
Anselme Irumva <anselme.irumva@tabiya.org>
2025-07-10 13:43:06 +0200

    fix(database): ensure explored_experiences field is populated or set to an empty list during state upgrade
    - because during the call stack, it is expected to be an array and not None

--- backend/app/store/_utils.py
+++ backend/app/store/_utils.py
@@ -3,16 +3,6 @@ from app.agent.explore_experiences_agent_director import DiveInPhase
 from app.application_state import ApplicationState
 
 
-def has_explored_experiences(state: ApplicationState) -> bool:
-    """
-    Check if some explored experiences in the state.
-    """
-    experiences = state.explore_experiences_director_state.experiences_state.values()
-    return any(
-        exp_state.dive_in_phase == DiveInPhase.PROCESSED
-        for exp_state in experiences
-    )
-
 def filter_explored_experiences(state: ApplicationState) -> list[ExperienceEntity]:
     """
     Populate explored_experiences based on experiences_state
