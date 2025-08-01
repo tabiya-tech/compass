831ce425df87d199a00eae01546d0b82b6f1f41c
Anselme Irumva <anselme.irumva@tabiya.org>
2025-08-01 08:48:37 +0200

    wip: skills ranking service

---
+++ backend/features/skills_ranking/ranking_service/repositories/default_collections.py
@@ -0,0 +1,10 @@
+class DefaultCollections:
+    OPPORTUNITIES_DATA = "opportunities_data"
+    """
+    Default collection name for opportunities data.
+    """
+
+    JOB_SEEKERS_DATA = "job_seekers_data"
+    """
+    Default collection name for job seekers data.
+    """
