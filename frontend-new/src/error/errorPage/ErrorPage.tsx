203002bb362839cdadc5a19f7d34d148f58a360f
nraffa <nraffapirra@gmail.com>
2025-10-09 17:52:20 -0300

    feat(i18n): integrate i18next for error handling and add translations for error messages
    fix translation files conflicts

--- frontend-new/src/error/errorPage/ErrorPage.tsx
+++ frontend-new/src/error/errorPage/ErrorPage.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import BugReportButton from "src/feedback/bugReport/bugReportButton/BugReportButton";
 
 const uniqueId = "37d307ae-4f1e-4d8d-bafe-fd642f8af4ab";
@@ -15,6 +16,7 @@ interface ErrorPageProps {
 }
 
 const ErrorPage: React.FC<ErrorPageProps> = ({ errorMessage }) => {
+  const { t } = useTranslation();
   return (
     <Box
       sx={{
@@ -29,7 +31,7 @@ const ErrorPage: React.FC<ErrorPageProps> = ({ errorMessage }) => {
     >
       <img
         src="/logo.svg"
-        alt="error illustration"
+        alt={t("error_page_illustration_alt")}
         width="250px"
         data-testid={DATA_TEST_ID.ERROR_ILLUSTRATION}
         style={{ marginBottom: "2rem" }}
