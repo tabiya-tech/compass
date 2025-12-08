d31098fa6ec32ec5b7010801d5211ae0e36a1127
Fides <noella.fides@tabiya.org>
2025-12-08 11:15:52 +0200

    chore: remove unused POC frontend folder from project
    (cherry picked from commit bd34320179db34b3ec44307455a0ac487b2b0b88)

--- frontend/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: process.env.NEXT_OUTPUT || 'standalone',
-    basePath: "/poc-ui",
-    reactStrictMode: false,
-};
-
-export default nextConfig;
