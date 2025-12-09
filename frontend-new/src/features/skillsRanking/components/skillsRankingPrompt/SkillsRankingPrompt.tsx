12a05b74870418126819451d35cc2c46fbcab5da
Bereket Terefe <bereket.terefe@tabiya.org>
2025-12-09 13:10:40 +0300

    feat(screening): change compensation values

--- frontend-new/src/features/skillsRanking/components/skillsRankingPrompt/SkillsRankingPrompt.tsx
+++ frontend-new/src/features/skillsRanking/components/skillsRankingPrompt/SkillsRankingPrompt.tsx
@@ -92,9 +92,9 @@ const SkillsRankingPrompt: React.FC<Readonly<SkillsRankingPromptProps>> = ({ onF
       <ChatBubble
         message={
           <Typography>
-            You are almost there! Remember that if you completely finish this conversation with me you will receive <strong>20-30 Rand in airtime.</strong>
+            You are almost there! Remember that if you completely finish this conversation with me you will receive <strong>30-40 Rand in airtime.</strong>
             <br />
-            Whether you receive 20 or 30 depends on your responses below, so please read carefully.
+            Whether you receive 30 or 40 depends on your responses below, so please read carefully.
           </Typography>
         }
         sender={ConversationMessageSender.COMPASS}
