eb13861423bfdebf2aad05f3f6a4c03989cb7368
Stevealila <stevealila25@gmail.com>
2025-12-14 07:47:56 +0300

    Remove dev txt files

--- backend/app/taxonomy/importers/__init__.py
+++ backend/app/taxonomy/importers/__init__.py
@@ -1,35 +1,3 @@
-'''How to run
-
-
-rm -rf ../../tabiya/resources/kesco_esco_hierarchical_matches.csv
-
-python3 -c "
-import asyncio
-from motor.motor_asyncio import AsyncIOMotorClient
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-async def clear():
-    client = AsyncIOMotorClient(os.getenv('TAXONOMY_MONGODB_URI'))
-    db = client[os.getenv('TAXONOMY_DATABASE_NAME')]
-    
-    for coll in ['occupations', 'skills', 'occupation_skill_relations']:
-        await db[coll].drop()
-        print(f'✓ Dropped {coll}')
-    
-    client.close()
-
-asyncio.run(clear())
-"
-
-python3 -m app.taxonomy.importers.run_all_imports
-
-
-
-'''
-
 from .esco_occupations_importer import ESCOOccupationsImporter
 from .esco_skills_importer import ESCOSkillsImporter
 from .esco_relations_importer import ESCORelationsImporter
